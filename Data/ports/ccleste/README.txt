c89917bc6937aba04f0ce26c2478939f5d17769b
cizia64 <cizia64@gmail.com>
2024-05-30 01:26:32 +0200

    Create all the Roms subfolders + adding a port demo
    port demo : celeste classic

---
+++ Data/ports/ccleste/README.txt
@@ -0,0 +1,28 @@
+Celeste Classic
+
+Celeste Classic is a C source port of the original celeste (Celeste classic).
+
+
+Developer: lemon (https://github.com/lemon32767)
+URL: https://github.com/lemon32767/ccleste
+
+
+CONTROLS
+========
+
+Start/Select = Pause
+
+Start+Select = Exit
+
+Dpad  and Left stick = left/right and look up/down
+Right Stick = left/right
+
+A = Jump
+B = Dash
+Y = Hold to reset
+X = Toggle screenshake
+
+l1/l2 = Load state
+r1/r2 - Save state
+
+Start + Select = Exit
---
+++ Roms/PORTS/Celeste Classic.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+if [ -z ${TASKSET+x} ]; then
+  source $controlfolder/tasksetter
+fi
+
+get_controls
+
+## TODO: Change to PortMaster/tty when Johnnyonflame merges the changes in,
+CUR_TTY=/dev/tty0
+
+PORTDIR="/$directory/ports"
+GAMEDIR="$PORTDIR/ccleste"
+cd $GAMEDIR
+
+$ESUDO chmod 666 /dev/uinput
+$ESUDO chmod 666 $CUR_TTY
+$ESUDO touch log.txt
+$ESUDO chmod 666 log.txt
+export TERM=linux
+printf "\033c" > $CUR_TTY
+
+## RUN SCRIPT HERE
+
+echo "Starting game." > $CUR_TTY
+
+$GPTOKEYB "ccleste" -c ccleste.gptk &
+$TASKSET ./ccleste 2>&1 | $ESUDO tee -a ./log.txt
+
+$ESUDO kill -9 $(pidof gptokeyb)
+unset LD_LIBRARY_PATH
+unset SDL_GAMECONTROLLERCONFIG
+$ESUDO systemctl restart oga_events &
+
+# Disable console
+printf "\033c" > $CUR_TTY
+
