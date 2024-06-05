1f451f586e071134c0c87c35dc5de925fa82a665
cizia64 <cizia64@gmail.com>
2024-06-05 19:27:24 +0200

    Fix & improve 3DO
    - Fix 3DO path (can't start with a digit)
    - set overlay
    - threaded dsp helps a lot (still not full speed) but can also triggers some hangs

---
+++ Emus/PANASONIC/performance.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
