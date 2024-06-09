473cbe0a30fa62be9e014acbd2e24a9f7fea90cb
cizia64 <cizia64@gmail.com>
2024-06-10 01:35:15 +0200

    Faster execution for scripts from System Tools
    25% faster to apply Overlays selector.
    When MainUI goes back the default speed is restored.

--- Emus/_SystemTools/launch.sh
+++ Emus/_SystemTools/launch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 echo $0 $*
 
+echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1416000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+
 # Launch tool script file
-echo "******************************************************************-*-*- $1"
+echo "*** Launching $1 ***"
 "$1" 
 
 # we don't memorize System Tools scripts in recent list
