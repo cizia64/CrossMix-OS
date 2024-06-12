d504e1c51ea92c00f93d385e0218e3fb90f6251e
cizia64 <cizia64@gmail.com>
2024-06-13 01:14:32 +0200

    Update PPSSPP with hotfix 9
    PSP folder reorganized, 3 versions of PPSSPP:
    
    - PPSSPP 1.17.1 - OpenGL
    - PPSSPP 1.17.1 - Vulkan
    - PPSSPP-1.5.4
    
    Hotfix 9 observations:
    The 1.5.4 has not changed since hotfix 6.
    There are a little difference in the vulkan binary compared to hotfix 6.
    It now includes a GL version of the 1.17.1 (however it's potentially possible to use GL with the vulkan binary if you set the right option in ppsspp.ini).
    
    1.17.1 - OpenGL &  Vulkan now share the same config folder, it allows to avoid to remake the game configuration each time you switch between GL and Vulkan. Also the save states are mutualized between both version.
    
    The performance mode is enabled by detection of the label which have been selected in launcher menu, a script which could be useful for other use cases. It avoid to have a lot of duplicated launcher scripts.

--- Emus/PSP/cpufreq.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1416000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
---
+++ Emus/PSP/launch_1.17.1_vulkan.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+echo $0 $*
+progdir=`dirname "$0"`
+progdir171=$progdir/PPSSPP_1.17.1
+cd "$progdir171"
+
+echo "=============================================="
+echo "==================== PPSSPP  ================="
+echo "=============================================="
+
+performance=$(grep -i "dowork 0x" "/tmp/log/messages" | tail -n 1 | grep -i "Perf.") # We detect the performance mode from the label which have been selected in launcher menu
+if [ -n "$performance" ]; then
+    echo "Performance mode selected"
+	echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+	echo 1 > /sys/devices/system/cpu/cpu0/online
+	echo 1 > /sys/devices/system/cpu/cpu1/online
+	echo 1 > /sys/devices/system/cpu/cpu2/online
+	echo 1 > /sys/devices/system/cpu/cpu3/online
+fi
+
+# We set the Backend to Vulkan
+config_file="/mnt/SDCARD/Emus/PSP/PPSSPP_1.17.1/.config/ppsspp/PSP/SYSTEM/ppsspp.ini"
+sed -i '/^\[Graphics\]$/,/^\[/ s/GraphicsBackend = .*/GraphicsBackend = 3/' "$config_file"
+
+export HOME=$progdir171
+#export SDL_AUDIODRIVER=dsp   //disable 20231031 for sound suspend issue
+./PPSSPPSDL_vulkan "$*"
