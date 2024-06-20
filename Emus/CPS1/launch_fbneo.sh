5b05df565f8344fa044ad01259aead459434c31f
cobaltgit <cobaltmail4204@gmail.com>
2024-06-20 13:26:43 +0100

    Add fbneo launch options to cps1 and cps2

---
+++ Emus/CPS1/launch_fbneo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/CPS1
+cd $RA_DIR/
+
+$EMU_DIR/cpufreq.sh
+
+#force using fbneo
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/fbneo_libretro.so "$@"
