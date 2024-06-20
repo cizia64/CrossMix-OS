2f4bfdabf5339e881190979629c5283940edfbdc
cizia64 <cizia64@gmail.com>
2024-06-21 00:00:15 +0200

    Dreamcast organization
    - Add dreamcast overlay (max ratio)
    - Add widescreen mode
    - create directory override instead of core override
    - Remove old not used flycast cores

---
+++ Emus/DC/flycast.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo $0 $*
+source /mnt/SDCARD/System/usr/trimui/scripts/FolderOverrideFinder.sh
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/DC
+
+$EMU_DIR/performance.sh
+$EMU_DIR/effect.sh
+
+cd $RA_DIR/
+
+#disable netplay
+NET_PARAM=
+
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/flycast_libretro.so "$@"
