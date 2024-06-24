902707278331ce742f4184c6e912a97bb74b58e6
cizia64 <cizia64@gmail.com>
2024-06-24 14:49:12 +0200

    Add Samba/SMB/CIFS server (network share)
    Add Samba/SMB/CIFS server (network share)
    
    Based on Batocera 32 binaries
    Launch it from "System Tools" app.
    2 modes : with or without password (use this one in case of difficulities to logon)
    System Tool scripts and graphic assets added
    Autorun at boot once enabled
    
    To access: type \\x.x.x.x in your Windows Explorer address bar (where x.x.x.x is the IP address of your TSP)
    
    diag notes:
    =======================================================================
    
    variable declarations
    ---------------------
    export LD_LIBRARY_PATH=/mnt/SDCARD/System/lib:/mnt/SDCARD/System/lib/samba
    PATH="/mnt/SDCARD/System/bin:$PATH"
    CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb-secure.conf"
    
    list SMB users
    --------------
    pdbedit -L -s ${CONFIGFILE}
    
    check configuration file
    ------------------------
    testparm  ${CONFIGFILE}
    
    test smb connection
    -------------------
    smbclient //localhost/sdcard -U root --configfile=${CONFIGFILE}
    
    Useful commands on Windows side
    ---------------------------------
    The first good reflex if you have difficulties to logon is rebooting your Windows
    net use /d * /y   (then restart explorer.exe)
    rundll32.exe keymgr.dll,KRShowKeyMgr  -> to clean the recorded password entry
    net view \\x.x.x.x  -> to see the current shares
    Set-SmbClientConfiguration -RequireSecuritySignature $false   -> if you have authentification problems on Windows 10

--- System/starts/°post_starts.sh
+++ System/starts/°post_starts.sh
@@ -1,3 +1,6 @@
+PATH="/mnt/SDCARD/System/bin:$PATH"
+export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/mnt/SDCARD/System/lib/samba:/usr/trimui/lib:$LD_LIBRARY_PATH"
+
 TELNET_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["TELNET"]' "/mnt/SDCARD/System/etc/crossmix.json")
 if [ "$TELNET_enabled" -eq 1 ]; then
 	telnetd
@@ -7,4 +10,22 @@ Syncthing_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["Syncthing"]' "/mnt/SDCARD/S
 if [ "$Syncthing_enabled" -eq 1 ]; then
 	CONFIGPATH=/mnt/SDCARD/System/etc/syncthing
 	/mnt/SDCARD/System/bin/syncthing serve --no-restart --no-upgrade --config="$CONFIGPATH" --data="$CONFIGPATH/data" &
+fi
+
+smb_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMB"]' "/mnt/SDCARD/System/etc/crossmix.json")
+if [ "$smb_enabled" -eq 1 ]; then
+	rm -rf /var/cache/samba /var/log/samba /var/lock/subsys /var/run/samba /var/lib/samba/
+	mkdir -p /var/cache/samba /var/log/samba /var/lock/subsys /var/run/samba /var/run/samba/locks /var/lib/samba/private
+
+	smb_secure_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMB_secure"]' "/mnt/SDCARD/System/etc/crossmix.json")
+	if [ "$smb_secure_enabled" -eq 1 ]; then
+		CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb-secure.conf"
+		echo -e "trimui\ntrimui\n" | smbpasswd -s -a root -c ${CONFIGFILE}
+	else
+		CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb.conf"
+	fi
+
+	smbd -s ${CONFIGFILE} -D
+	nmbd -D --configfile="${CONFIGFILE}"
+
 fi
