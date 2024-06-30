2e87dcaabd69cc547aa319cc1b59bf91fcec4831
cobaltgit <cobaltmail4204@gmail.com>
2024-06-30 22:33:03 +0100

    Add standalone Mupen64Plus emulator Credits: https://www.reddit.com/r/trimui/comments/1dsbm2x/mupen64_standalone_download/

---
+++ Emus/N64/mupen64plus_rice.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo $0 $*
+
+EMU_DIR=/mnt/SDCARD/Emus/N64/mupen64plus
+CONFDIR="$EMU_DIR/confr/"
+mkdir -p "$EMU_DIR/confr"
+export XDG_CONFIG_HOME="$CONFDIR"
+export XDG_DATA_HOME="$CONFDIR"
+export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
+
+./performance.sh
+PWD=$PWD
+cd $EMU_DIR/
+
+PATH=$PWD:$EMU_DIR:$PATH
+export LD_LIBRARY_PATH=$PWD/libs:$EMU_DIR/libs:$LD_LIBRARY_PATH
+
+echo $EMU_DIR/gptokeyb -k mupen64plus -c "./defkeys.gptk" 
+$EMU_DIR/gptokeyb -k mupen64plus -c "./defkeys.gptk" &
+
+./mupen64plus "$*" 2>&1 > /mnt/SDCARD/mupen-log.txt
+
+$ESUDO kill -9 $(pidof gptokeyb)
