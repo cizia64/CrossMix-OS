b0dd21feccde9e7026a7b8914ec267df97368157
cizia64 <cizia64@gmail.com>
2024-07-03 18:40:27 +0200

    Make "User Guide" app theme-dependant
    - Make "User Guide" app theme-dependant
    - Add "CrossMix Refreshed" assets for "User Guide" app

--- Apps/user_guide/launch.sh
+++ Apps/user_guide/launch.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo $0 $*
-progdir=`dirname "$0"`
-cd $progdir
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$progdir
-#HOME=progdir $progdir/retroarch -v
+progdir=$(pwd)
 
-./user_guide
+CrossMix_Theme=$(/mnt/SDCARD/System/bin/jq -r '.["CrossMix Theme"]' "/mnt/SDCARD/System/etc/crossmix.json")
+
+if [ -d "$progdir/theme_$CrossMix_Theme" ]; then
+	cd "$progdir/theme_$CrossMix_Theme"
+else
+	cd "$progdir"
+fi
+
+"$progdir/user_guide"
