21e7e2951894c4fbba8157aa3431c3a9e5718c4a
cobaltgit <cobaltmail4204@gmail.com>
2024-07-08 12:18:06 +0100

    Change performance profile and default launch

--- Emus/N64/performance.sh
+++ Emus/N64/performance.sh
@@ -1,2 +1,3 @@
-echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 408000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
