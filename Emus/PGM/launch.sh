dea5e9f3d1a7bfeb0340e2e0304df3d047471f53
cizia64 <cizia64@gmail.com>
2024-07-08 02:37:59 +0200

    Add GEOLITH & FBneo emualtor choices.
    * Add fbneo launch option to cps1, cps2 and cps3
    * Same goes for polygame master
    * Add Geolith launch option
    
    Co-authored-by: cobaltgit <cobaltmail4204@gmail.com>
    Co-authored-by: Cobalt <65132371+cobaltgit@users.noreply.github.com>

--- Emus/PGM/launch.sh
+++ Emus/PGM/launch.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+source /mnt/SDCARD/System/usr/trimui/scripts/FolderOverrideFinder.sh
 RA_DIR=/mnt/SDCARD/RetroArch
 EMU_DIR=/mnt/SDCARD/Emus/PGM
 cd $RA_DIR/
+
+$EMU_DIR/cpufreq.sh
 
 HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/fbalpha2012_libretro.so "$@"
 #HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $EMU_DIR/fbneo_libretro.so "$@"
