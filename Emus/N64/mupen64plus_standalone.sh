bcd024fd526444efbb79fcfa71039216e548bc05
cizia64 <cizia64@gmail.com>
2024-07-08 18:48:22 +0200

    Add standalone Mupen64Plus emulator (#78)
    Credits: Rob-Retro-Linux (reddit post: https://www.reddit.com/r/trimui/comments/1dsbm2x/mupen64_standalone_download/)
    - Add standalone Mupen64Plus emulator
    - add support for support .zip and .7z files
     - Add optional 16:9 wide mode for glide64mk2
    - config optimization
    - scripts optimizations
    
    ---------
    
    Co-authored-by: cobaltgit

---
+++ Emus/N64/mupen64plus_standalone.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+echo $0 $*
+
+EMU_DIR=/mnt/SDCARD/Emus/N64/mupen64plus
+CONFDIR="/mnt/SDCARD/Emus/N64"
+export XDG_CONFIG_HOME="$CONFDIR"
+export XDG_DATA_HOME="$CONFDIR"
+export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
+
+PATH=$EMU_DIR:$PATH
+export LD_LIBRARY_PATH=$EMU_DIR:$EMU_DIR/libs:$LD_LIBRARY_PATH
+
+cd "$EMU_DIR"
+VideoPlugin=$(grep -i "dowork 0x" "/tmp/log/messages" | tail -n 1 | grep -i "Rice") # We detect the performance mode from the label which have been selected in launcher menu
+if [ -n "$VideoPlugin" ]; then
+    /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "$EMU_DIR/mupen64plus.cfg" "VideoPlugin" "mupen64plus-video-rice.so"
+else
+    VideoPlugin=$(grep -i "dowork 0x" "/tmp/log/messages" | tail -n 1 | grep -i "glide64mk2 - wide") # We detect the performance mode from the label which have been selected in launcher menu
+    if [ -n "$VideoPlugin" ]; then
+        /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "$EMU_DIR/mupen64plus.cfg" "aspect" "1"
+    else
+        /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "$EMU_DIR/mupen64plus.cfg" "aspect" "0"
+    fi
+    /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "$EMU_DIR/mupen64plus.cfg" "VideoPlugin" "mupen64plus-video-glide64mk2.so"
+fi
+
+case "$*" in
+*.n64 | *.v64 | *.z64)
+    ROM_PATH="$*"
+    ;;
+*.zip | *.7z)
+    echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo 1800000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+    TEMP_ROM=$(mktemp)
+    ROM_PATH="$TEMP_ROM"
+    /mnt/SDCARD/System/bin/7zz e "$*" -so >"$TEMP_ROM"
+    ;;
+esac
+
+../performance.sh
+
+/mnt/SDCARD/Apps/PortMaster/PortMaster/gptokeyb2 -c "./defkeys.gptk" &
+sleep 0.3
+
+HOME="$EMU_DIR" ./mupen64plus "$ROM_PATH"
+
+rm -f "$TEMP_ROM"
+
+kill -9 $(pidof gptokeyb)
--- Emus/N64DD/launch.sh
+++ Emus/N64DD/launch.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
+
 echo $0 $*
-source /mnt/SDCARD/System/usr/trimui/scripts/FolderOverrideFinder.sh
-RA_DIR=/mnt/SDCARD/RetroArch
-EMU_DIR=/mnt/SDCARD/Emus/N64DD
 
-$EMU_DIR/performance.sh
+EMU_DIR=/mnt/SDCARD/Emus/N64/mupen64plus
+CONFDIR="$EMU_DIR/conf/"
+mkdir -p "$EMU_DIR/conf"
+export XDG_CONFIG_HOME="$CONFDIR"
+export XDG_DATA_HOME="$CONFDIR"
+export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
 
-cd $RA_DIR/
+./performance.sh
+PWD=$PWD
+cd $EMU_DIR/
 
-#disable netplay
-NET_PARAM=
+PATH=$PWD:$EMU_DIR:$PATH
+export LD_LIBRARY_PATH=$PWD/libs:$EMU_DIR/libs:$LD_LIBRARY_PATH
 
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/mupen64plus_libretro.so "$@"
+case "$*" in
+    *.n64|*.v64|*.z64|*.ndd) 
+        ROM_PATH="$*" 
+        ;;
+    *.zip|*.7z)
+        TEMP_ROM=$(mktemp)
+        ROM_PATH="$TEMP_ROM"
+        /mnt/SDCARD/System/bin/7zz e "$*" -so > "$TEMP_ROM"
+        ;;
+esac
+
+echo $EMU_DIR/gptokeyb -k mupen64plus -c "./defkeys.gptk" 
+$EMU_DIR/gptokeyb -k mupen64plus -c "./defkeys.gptk" &
+
+./mupen64plus "$ROM_PATH" 2>&1
+
+rm -f "$TEMP_ROM"
+
+$ESUDO kill -9 $(pidof gptokeyb)
