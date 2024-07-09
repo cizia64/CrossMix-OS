398a0ddfbf5caa88f4eb150ab37113872a37520c
cobaltgit <cobaltmail4204@gmail.com>
2024-07-09 12:36:07 +0100

    Change performance profile Ondemand governor causes framedrops due to flycast being cpu-heavy

--- Emus/NAOMI/cpufreq.sh
+++ Emus/NAOMI/cpufreq.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
