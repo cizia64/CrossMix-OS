f4d45effb85a739087b0bfefb33fc820f842ed6e
cizia64 <cizia64@gmail.com>
2024-07-09 02:40:16 +0200

    Update Ambilight.sh
    - fix binaries path
    - use infoscreen.sh and mainui_state_update.sh

--- Apps/SystemTools/Menu/LEDS (value)/Ambilight.sh
+++ Apps/SystemTools/Menu/LEDS (value)/Ambilight.sh
@@ -2,12 +2,7 @@
 PATH="/mnt/SDCARD/System/bin:$PATH"
 LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/usr/trimui/lib:$LD_LIBRARY_PATH"
 
-/mnt/SDCARD/System/bin/sdl2imgshow \
-    -i "/mnt/SDCARD/trimui/res/crossmix-os/bg-info.png" \
-    -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
-    -s 50 \
-    -c "220,220,220" \
-    -t "Applying \"$(basename "$0" .sh)\" mode..." &
+/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "Applying \"$(basename "$0")\" by default..."
 
 output_file="/mnt/SDCARD/System/etc/led_config.sh"
 ScriptName=$(basename "$output_file")
@@ -24,20 +19,16 @@ echo 10 > /sys/class/led_anim/effect_duration_lr
 
 
 while true; do
-    /mnt/SDCARD/bin/fb2png -p /tmp/fb1.png -s8 -t8 -z2 -x320 -y180 -w640 -h360 -s2 -t2 -z2
-    color=$(/mnt/SDCARD/System/bin/python3.11 /mnt/SDCARD/bin/colorthief.py /tmp/fb1.png)
+    /mnt/SDCARD/System/bin/fb2png -p /tmp/fb1.png -s8 -t8 -z2 -x320 -y180 -w640 -h360 -s2 -t2 -z2
+    color=$(/mnt/SDCARD/System/bin/python3.11 /mnt/SDCARD/System/bin/colorthief.py /tmp/fb1.png)
     echo $color > /sys/class/led_anim/effect_rgb_hex_lr
     echo 1 > /sys/class/led_anim/effect_lr
     sleep 0.05
 done
 
-
-
-
 EOF
 }
 
-echo "====================================== $ScriptName "
 LedLoop >"$output_file"
 
 pkill -f "led_config.sh"
@@ -53,14 +44,4 @@ if [ ! -f "$json_file" ]; then
 fi
 /mnt/SDCARD/System/bin/jq --arg script_name "$script_name" '. += {"LEDS": $script_name}' "$json_file" >"/tmp/json_file.tmp" && mv "/tmp/json_file.tmp" "$json_file"
 
-database_file="/mnt/SDCARD/Apps/SystemTools/Menu/Menu_cache7.db"
-
-sqlite3 "$database_file" "UPDATE Menu_roms SET disp = 'LEDS ($script_name)',pinyin = 'LEDS ($script_name)',cpinyin = 'LEDS ($script_name)',opinyin = 'LEDS ($script_name)' WHERE disp LIKE 'LEDS (%)';"
-sqlite3 "$database_file" "UPDATE Menu_roms SET ppath = 'LEDS ($script_name)' WHERE ppath LIKE 'LEDS (%)';"
-json_file="/tmp/state.json"
-
-jq --arg script_name "$script_name" '.list |= map(if (.ppath | index("LEDS ")) then .ppath = "LEDS (\($script_name))" else . end)' "$json_file" >"$json_file.tmp" && mv "$json_file.tmp" "$json_file"
-
-sync
-sleep 0.1
-pkill -f sdl2imgshow
+/mnt/SDCARD/System/usr/trimui/scripts/mainui_state_update.sh "LEDS" "$script_name"
