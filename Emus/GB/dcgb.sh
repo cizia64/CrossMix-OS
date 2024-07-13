695dc780a72604c987cfb794d45f025bf86ae54e
cobaltgit <cobaltmail4204@gmail.com>
2024-07-13 16:55:33 +0100

    Add DoubleCherryGB core

---
+++ Emus/GB/dcgb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "===================================="
+echo $0 $*
+source /mnt/SDCARD/System/usr/trimui/scripts/FolderOverrideFinder.sh
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/GB
+cd $RA_DIR/
+
+#disable netplay
+NET_PARAM=
+
+
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/DoubleCherryGB_libretro.so "$@"
+
---
+++ Emus/GBC/dcgb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "===================================="
+echo $0 $*
+source /mnt/SDCARD/System/usr/trimui/scripts/FolderOverrideFinder.sh
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/GBC
+cd $RA_DIR/
+
+$EMU_DIR/cpufreq.sh
+
+#disable netplay
+NET_PARAM=
+
+
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/DoubleCherryGB_libretro.so "$@"
+
