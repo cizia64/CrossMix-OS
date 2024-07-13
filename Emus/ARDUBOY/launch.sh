3a4e6f9dd8912b00b532b51f084df4f7b956bf77
cobaltgit <cobaltmail4204@gmail.com>
2024-07-13 16:52:28 +0100

    Add Ardens core for Arduboy

--- Emus/ARDUBOY/launch.sh
+++ Emus/ARDUBOY/launch.sh
@@ -9,7 +9,13 @@ cd $RA_DIR/
 #disable netplay
 NET_PARAM=
 
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/arduous_libretro.so "$@"
+if grep -i "dowork 0x" "/tmp/log/messages" | tail -n 1 | grep -iq "Ardens"; then
+    CORE_FILE="$RA_DIR/.retroarch/cores/ardens_libretro.so"
+else
+    CORE_FILE="$RA_DIR/.retroarch/cores/arduous_libretro.so"
+fi
+
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L "$CORE_FILE" "$@"
 
 #HOME=$RA_DIR/ $RA_DIR/retroarch -v $NET_PARAM -L $RA_DIR/.retroarch/cores/arduous_libretro.so "$@"
 #HOME=$RA_DIR/ $RA_DIR/retroarch -v $NET_PARAM -L $EMU_DIR/arduous_libretro.so "$@"
