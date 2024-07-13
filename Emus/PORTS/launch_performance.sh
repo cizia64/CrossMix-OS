db5842fa16746baaaab7c171478647a034f00675
cobaltgit <cobaltmail4204@gmail.com>
2024-07-13 13:31:35 +0100

    Include crossmix libs in port launch script

--- Emus/PORTS/launch_performance.sh
+++ Emus/PORTS/launch_performance.sh
@@ -9,4 +9,5 @@ echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 PORTS_DIR=/mnt/SDCARD/Roms/PORTS
 cd $PORTS_DIR/
 
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/SDCARD/System/lib"
 /bin/sh "$@"
