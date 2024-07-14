fcea14834a3ceea052bd7a28999058170393b09f
cizia64 <cizia64@gmail.com>
2024-07-14 17:13:00 +0200

    Update FNEO cpufreq.sh

--- Emus/FBNEO/cpufreq.sh
+++ Emus/FBNEO/cpufreq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1416000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo 1800000 >/sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1416000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
