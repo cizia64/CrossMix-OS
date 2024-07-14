5c4ea875bca48c1d256a5772d3423875d82ec5e6
cizia64 <cizia64@gmail.com>
2024-07-14 04:39:15 +0200

    Workaround for ports controls problems
    This is really a strange behavior from the stock firmware: Input devices are not "hotplug": I have remarked that if you plug any input device when an app is already running, the input device will not be detected.
    For example on MainUI (or any other app like a port), plug an USB keyboard: it doesn't work until you restart MainUI. The problem is not here on Knulli.  gptokeyb creates virtual input devices often after the launch of the port so it's the same problem.

--- Emus/PORTS/launch.sh
+++ Emus/PORTS/launch.sh
@@ -17,4 +17,31 @@ PORTS_DIR=/mnt/SDCARD/Roms/PORTS
 cd "$PORTS_DIR"
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/SDCARD/System/lib"
+
+################ Fix for TSP controls ################
+
+FILE="$@"
+LINE_TO_ADD="sleep 0.3 # For TSP only, do not move/modify this line."
+
+# Check if the line already exists
+if ! grep -q "$LINE_TO_ADD" "$FILE"; then
+    # Use awk to insert the line after the target line only if it doesn't already exist
+    awk -v line="$LINE_TO_ADD" '
+    BEGIN { line_inserted = 0 }
+    /^[[:space:]]*\$GPTOKEYB[[:space:]]*.*&[[:space:]]*$/ {
+        print $0
+        if (!line_inserted) {
+            print line
+            line_inserted = 1
+        }
+        next
+    }
+    { print $0 }
+    ' "$FILE" > /tmp/port_tmp.sh && mv /tmp/port_tmp.sh "$FILE"
+fi
+sync
+
+######################################################
+
+
 /bin/sh "$@"
