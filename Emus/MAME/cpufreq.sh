ae98db689afe3a608fb9bc0096dafa641a4c5a94
cobaltgit <cobaltmail4204@gmail.com>
2024-07-14 16:51:41 +0100

    [Emus] MAME launcher cleanup

--- Emus/MAME/cpufreq.sh
+++ Emus/MAME/cpufreq.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 2000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
