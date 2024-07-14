45453e31e395f268debe12b1e2f72a9ccd7fd1a8
cizia64 <cizia64@gmail.com>
2024-07-14 15:52:33 +0200

    Fix SFC cpufreq.sh
    https://github.com/cizia64/CrossMix-OS/issues/92

--- Emus/SFC/cpufreq.sh
+++ Emus/SFC/cpufreq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1416000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 1800000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
