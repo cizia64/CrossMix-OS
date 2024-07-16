ca7c9bf36079002f61ed6373432f5ff53a61bb5b
cizia64 <cizia64@gmail.com>
2024-07-16 23:01:01 +0200

    Modify non-standard frequencies

--- Emus/NDS/performance.sh
+++ Emus/NDS/performance.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1500000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1608000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
