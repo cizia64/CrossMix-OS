617b3f958cf9d4eb734642110ab73d1314644ce6
cizia64 <cizia64@gmail.com>
2024-07-17 15:40:20 +0200

    Performance tuning
    I know that scaling_min_freq is useless in performance mode, these are old relics from tomato scripts.
    
    I'll make a global review of all perf modes later.

--- Emus/MAME2003PLUS/cpufreq.sh
+++ Emus/MAME2003PLUS/cpufreq.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 1608000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1608000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
