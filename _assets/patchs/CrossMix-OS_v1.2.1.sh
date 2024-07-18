61aeb0b722037c9a1ea983c464539efa03cf9b64
cizia64 <cizia64@gmail.com>
2024-07-18 12:46:52 +0200

    Update CrossMix-OS_v1.2.1.sh
    - Add reboot on successful patch
    - Avoid to apply the patch if not necessary

--- _assets/patchs/CrossMix-OS_v1.2.1.sh
+++ _assets/patchs/CrossMix-OS_v1.2.1.sh
@@ -11,14 +11,20 @@ BOLD='\033[1m'
 UNDERLINE='\033[4m'
 BLINK='\x1b[5m'
 
+Current_asound_conf=$(crc32 "/etc/asound.conf" | awk '{print $1}')
+
+if [ "$Current_asound_conf" = "d6a69715" ]; then
+	echo -e "${GREEN}Patch not necessary or already applied.${NC}"
+	exit
+fi
+
 GITHUB_REPOSITORY=cizia64/CrossMix-OS
 version=$(cat /mnt/SDCARD/System/usr/trimui/crossmix-version.txt)
 url="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/main/_assets/patchs/CrossMix-OS_v$version.zip"
 
-
 urlcmd=$(echo "wget  "$url" -O \"/tmp/CrossMix-OS_v$version.zip\"")
 echo $urlcmd >/tmp/rundl.sh
-sh /tmp/rundl.sh  >/dev/null 2>&1
+sh /tmp/rundl.sh >/dev/null 2>&1
 
 if [ -f "/tmp/CrossMix-OS_v$version.zip" ]; then
 	echo -e "${GREEN}Patch download OK!${NONE}"
@@ -27,10 +33,17 @@ if [ -f "/tmp/CrossMix-OS_v$version.zip" ]; then
 	sync
 	cp "/mnt/SDCARD/System/usr/trimui/etc/asound.conf" "/etc/asound.conf"
 	if [ $? -eq 0 ]; then
-	  echo -e "${GREEN}Patch v$version successful.${NC}"
-	  rm "/tmp/CrossMix-OS_v$version.zip"
+		echo -e "${GREEN}Patch v$version successful. Rebooting now${NC}"
+		rm "/tmp/CrossMix-OS_v$version.zip"
+		sleep 5
+		if [ -f /mnt/SDCARD/System/bin/shutdown ]; then
+			/mnt/SDCARD/System/bin/shutdown -r
+		else
+			reboot &
+		fi
+		sleep 30
 	else
-	  echo -ne "${RED}Patch v$version failed.${NC}\n"
+		echo -ne "${RED}Patch v$version failed.${NC}\n"
 	fi
 else
 	echo -e "${RED}Patch download failed.${NONE}"
