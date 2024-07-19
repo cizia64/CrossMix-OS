db21a5a4a242ceae15fb421e66e2ca21a15fa4bc
cobaltgit <cobaltmail4204@gmail.com>
2024-07-19 13:16:28 +0100

    [Fix] Fix restore retroarch defaults script

--- Apps/SystemTools/Menu/EMULATORS/Reset Retroarch Configuration.sh
+++ Apps/SystemTools/Menu/EMULATORS/Reset Retroarch Configuration.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/usr/trimui/lib:$LD_LIBRARY_PATH"
+RA_DEFAULT_CONFIG="/mnt/SDCARD/RetroArch/default_config.7z"
 
 button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "Restore Retroarch: press A to continue, B to cancel." -k "A B")
 
@@ -11,6 +12,7 @@ fi
 
 /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "Restoring Retroarch configuration..."
 
-unzip -o /mnt/SDCARD/RetroArch/default_config.7z -d /mnt/SDCARD/
+/mnt/SDCARD/System/bin/7zz e $RA_DEFAULT_CONFIG -y -o/mnt/SDCARD/RetroArch retroarch.cfg
+/mnt/SDCARD/System/bin/7zz x $RA_DEFAULT_CONFIG -y -o/mnt/SDCARD/RetroArch/.retroarch/config -x!retroarch.cfg 
 
 sleep 0.1
