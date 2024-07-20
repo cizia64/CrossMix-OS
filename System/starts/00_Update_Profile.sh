94cc2aea513b19dae0acb3050c550b38f503f6b1
nevrdid <nevrdid@msn.com>
2024-07-21 01:00:38 +0200

    fix unwanted expansion

--- System/starts/00_Update_Profile.sh
+++ System/starts/00_Update_Profile.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 if ! grep -q "/mnt/SDCARD/System/bin" /etc/profile; then
-    echo "export PATH=\"/mnt/SDCARD/System/bin${PATH:+:$PATH}\"" >>/etc/profile
+    echo 'export PATH="/mnt/SDCARD/System/bin${PATH:+:$PATH}"' >>/etc/profile
 fi
 if ! grep -q "/mnt/SDCARD/System/lib" /etc/profile; then
-    echo "export LD_LIBRARY_PATH=\"/mnt/SDCARD/System/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}\"" >>/etc/profile
+    echo 'export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"' >>/etc/profile
 fi
 
