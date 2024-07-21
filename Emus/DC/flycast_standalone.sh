00e170e0fe7763693a6fc2f77c06e2b1cee70205
cobaltgit <cobaltmail4204@gmail.com>
2024-07-21 17:40:08 +0100

    Add KMFDManic MorpheusCast Xtreme emulator A very performance-optimised fork of Flycast that may sacrifice some accuracy/stability in a few games https://github.com/KMFDManic/MorpheusCast-Xtreme

--- Emus/DC/flycast_standalone.sh
+++ Emus/DC/flycast_standalone.sh
@@ -8,9 +8,16 @@ $EMU_DIR/effect.sh
 
 cd "$EMU_DIR/flycast"
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/SDCARD/System/lib:lib"
 export FLYCAST_BIOS_DIR="/mnt/SDCARD/BIOS/dc/"
 export FLYCAST_DATA_DIR=$FLYCAST_BIOS_DIR
 export FLYCAST_CONFIG_DIR="$EMU_DIR/flycast/config/"
 
-./flycast "$@"
+FLYCAST_BINARY="./flycast"
+
+if grep -i "dowork 0x" "/tmp/log/messages" | tail -n 1 | grep -iq "(KMFD Xtreme)"; then
+    echo "Flycast Xtreme selected"
+    FLYCAST_BINARY="${FLYCAST_BINARY}_xtreme"
+fi
+
+"$FLYCAST_BINARY" "$@" 
