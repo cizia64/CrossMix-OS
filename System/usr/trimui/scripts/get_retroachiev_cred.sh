9e9839fd54e49822fa98e779171eacb31bc9bc23
nevrdid <nevrdid@msn.com>
2024-07-21 02:18:12 +0200

    * Script to generate the token from crendentials set in retroarch. * The script also apply changes to ppsspp standalones configs.

---
+++ System/usr/trimui/scripts/get_retroachiev_cred.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+mkdir -p /mnt/SDCARD/tmp
+cd /mnt/SDCARD/tmp
+
+export PATH="$PATH:/mnt/SDCARD/System/usr/trimui/scripts"
+export HOME="/mnt/SDCARD/RetroArch"
+
+ConfigFile="./retroarch.cfg"
+
+cp /mnt/SDCARD/RetroArch/retroarch.cfg "$ConfigFile"
+
+Username=$(get_ra_cfg.sh "$ConfigFile" "cheevos_username")
+Password=$(get_ra_cfg.sh "$ConfigFile" "cheevos_password")
+
+if [ -z "$Username" ] || [ -z "$Password" ]; then
+	infoscreen.sh -m "Username or password not found! Please set them in RetroArch app first." -k "A B START MENU" -fs 30
+	exit 1
+fi
+
+set_ra_cfg.sh "$ConfigFile" "config_save_on_exit" "true"
+
+infoscreen.sh -m "A game will start and close to generate your token, please wait. Press A to continue." -k "A B START MENU" -fs 15
+$HOME/ra64.trimui -L "$HOME/.retroarch/cores/mgba_libretro.so" -c "$ConfigFile" "/mnt/SDCARD/Best/Free Games Collection/Roms/GBA/SpaceTwins.zip" &
+sleep 10
+pkill -f ra64.trimui
+
+Token=$(get_ra_cfg.sh "$ConfigFile" "cheevos_token")
+
+if [ -z "$Token" ]; then
+	infoscreen.sh -m "Failed to get cheevos token! Please check your username and password." -k "A B START MENU" -fs 30
+	exit 1
+fi
+
+# PPSSPP 1.17.1
+sed -i "s/AchievementsUserName =.*/AchievementsUserName = \"$Username\"/" /mnt/SDCARD/Emus/PSP/PPSSPP_1.17.1/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+echo "$Token" >/mnt/SDCARD/Emus/PSP/PPSSPP_1.17.1/.config/ppsspp/PSP/SYSTEM/ppsspp_retroachievements.dat
+
+# PPSSPP 1.15.4
+sed -i "s/AchievementsUserName =.*/AchievementsUserName = \"$Username\"/" /mnt/SDCARD/Emus/PSP/PPSSPP_1.15.4/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+echo "$Token" >/mnt/SDCARD/Emus/PSP/PPSSPP_1.15.4/.config/ppsspp/PSP/SYSTEM/ppsspp_retroachievements.dat
+
+rm -rf .retroarch
+rm -f content_*
+rm -f $ConfigFile
+
+infoscreen.sh -m "RetroAchievements credentials set successfully for PPSSPP!" -k "A B START MENU" -fs 30
+
+exit 0
