7f19f4010d61b6d444c150102371bb2ad999252a
nevrdid <nevrdid@msn.com>
2024-07-21 03:11:23 +0200

    - Check retroachievements.org acces first - accelerate process using grep and sed to get and set configurations values.

--- System/usr/trimui/scripts/get_retroachiev_cred.sh
+++ System/usr/trimui/scripts/get_retroachiev_cred.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if ! ping -q -c 1 -W 1 retroachievements.org > /dev/null; then
+    infoscreen.sh -m "No network connection! Please connect to the internet first." -k "A B START MENU" -fs 30
+    exit 1
+fi
+
 mkdir -p /mnt/SDCARD/tmp
 cd /mnt/SDCARD/tmp
 
@@ -10,23 +15,23 @@ ConfigFile="./retroarch.cfg"
 
 cp /mnt/SDCARD/RetroArch/retroarch.cfg "$ConfigFile"
 
-Username=$(get_ra_cfg.sh "$ConfigFile" "cheevos_username" | cut -d '"' -f 2)
-Password=$(get_ra_cfg.sh "$ConfigFile" "cheevos_password" | cut -d '"' -f 2)
+Username=$(grep "^cheevos_username" "$ConfigFile"| cut -d '"' -f 2)
+Password=$(grep "^cheevos_password" "$ConfigFile" | cut -d '"' -f 2)
 
 if [ -z "$Username" ] || [ -z "$Password" ]; then
 	infoscreen.sh -m "Username or password not found! Please set them in RetroArch app first." -k "A B START MENU" -fs 30
 	exit 1
 fi
 
-set_ra_cfg.sh "$ConfigFile" "config_save_on_exit" "true"
+sed -i "s/^config_save_on_exit.*/config_save_on_exit = \"true\"/" "$ConfigFile"
 
 infoscreen.sh -m "A game will start and close to generate your token, please wait. Press A to continue." -k "A B START MENU" -fs 15
 $HOME/ra64.trimui -L "$HOME/.retroarch/cores/mgba_libretro.so" -c "$ConfigFile" "/mnt/SDCARD/Best/Free Games Collection/Roms/GBA/SpaceTwins.zip" &
-sleep 10
+sleep 5
 pkill -f ra64.trimui
 sleep 5
 
-Token=$(get_ra_cfg.sh "$ConfigFile" "cheevos_token" | cut -d '"' -f 2)
+Token=$(grep "^cheevos_token" "$ConfigFile"| cut -d '"' -f 2)
 
 if [ -z "$Token" ]; then
 	infoscreen.sh -m "Failed to get cheevos token! Please check your username and password." -k "A B START MENU" -fs 30
@@ -34,7 +39,7 @@ if [ -z "$Token" ]; then
 fi
 
 # PPSSPP 1.17.1
-sed -i "s/AchievementsUserName =.*/AchievementsUserName = \"$Username\"/" /mnt/SDCARD/Emus/PSP/PPSSPP_1.17.1/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+sed -i "s/^AchievementsUserName =.*/AchievementsUserName = \"$Username\"/" /mnt/SDCARD/Emus/PSP/PPSSPP_1.17.1/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
 echo "$Token" >/mnt/SDCARD/Emus/PSP/PPSSPP_1.17.1/.config/ppsspp/PSP/SYSTEM/ppsspp_retroachievements.dat
 
 rm -rf .retroarch
