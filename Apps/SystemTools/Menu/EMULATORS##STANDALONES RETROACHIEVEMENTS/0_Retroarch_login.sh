812d80d6b7d05d75559c0dcdf88ca7a0d7406bd7
nevrdid <nevrdid@msn.com>
2024-07-22 18:39:08 +0200

    Fix retroarch login script

--- Apps/SystemTools/Menu/EMULATORS##STANDALONES RETROACHIEVEMENTS/0_Retroarch_login.sh
+++ Apps/SystemTools/Menu/EMULATORS##STANDALONES RETROACHIEVEMENTS/0_Retroarch_login.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env sh
 
+export PATH="$PATH:/mnt/SDCARD/System/usr/trimui/scripts"
 ConfigFile="/mnt/SDCARD/RetroArch/retroarch.cfg"
-
 
 Username=$(grep "^cheevos_username" "$ConfigFile" | cut -d '"' -f 2)
 
 if [ -n "$Username" ]; then
 
-	button=$(infoscreen.sh -m "Username $Username is already set.\n\
-Do you want to onverwrite it?\n\
+	button=$(
+		infoscreen.sh -m "Username $Username is already set.\
+Do you want to onverwrite it?\
 A: Yes; B: No" -k "A B" -fs 30
-)
-    if [ "$button" = "B" ]; then
-        exit 0
-    fi
+	)
+	if [ "$button" = "B" ]; then
+		exit 0
+	fi
 fi
 
-commands=$(
-	cat <<EOF
+infoscreen.sh -m "Terminal will open to set your credentials. Press X to open keyboard and L to shift." -k "A B START MENU" -fs 30
+
+cat <<'EOF' >/tmp/tmp.sh
+#!/usr/bin/env sh
+
 escape_string() {
     echo -n "$1" | sed 's/\\/\\\\/g; s/"/\\"/g; s/'\''/\\'\''/g; s/ /\\ /g'
 }
+
+ConfigFile="/mnt/SDCARD/RetroArch/retroarch.cfg"
+
 echo "Please enter your RetroAchievements username:"
 read -r tmp
 Username=$(escape_string "$tmp")
@@ -29,7 +36,9 @@ echo "Please enter your RetroAchievements password:"
 read -r tmp
 Password=$(escape_string "$tmp")
 sed -i "s/^cheevos_password.*/cheevos_password = \"$Password\"/" "$ConfigFile"
+exit 0
 EOF
-)
+chmod +x /tmp/tmp.sh
 
-/mnt/SDCARD/Apps/Terminal/SimpleTerminal -e sh -c "$commands"
+/mnt/SDCARD/Apps/Terminal/SimpleTerminal -e "/tmp/tmp.sh"
+rm -f /tmp/tmp.sh
