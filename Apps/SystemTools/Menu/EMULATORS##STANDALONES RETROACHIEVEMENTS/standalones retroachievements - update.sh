c1c61831ff5cdfb1ed41f1277c9728afe83afcc0
nevrdid <nevrdid@msn.com>
2024-07-22 13:04:31 +0200

    Add an infoscreen to remind that a first login must be done in retroarch

--- Apps/SystemTools/Menu/EMULATORS##STANDALONES RETROACHIEVEMENTS/standalones retroachievements - update.sh
+++ Apps/SystemTools/Menu/EMULATORS##STANDALONES RETROACHIEVEMENTS/standalones retroachievements - update.sh
@@ -3,6 +3,14 @@
 export PATH="$PATH:/mnt/SDCARD/System/usr/trimui/scripts"
 export HOME="/mnt/SDCARD/RetroArch"
 
+button=$(infoscreen.sh -m "You will login to retroachievements with ppsspp standalone.\n\
+This require that you have already setup your username and password in retroarch\n\
+Press A to continue, B to cancel operation." -k "A B" -fs 30)
+
+if [ "$button" = "B" ]; then
+    exit 0
+fi
+
 if ! ping -q -c 1 -W 1 retroachievements.org > /dev/null; then
     infoscreen.sh -m "No network connection! Please connect to the internet first." -k "A B START MENU" -fs 30
     exit 1
