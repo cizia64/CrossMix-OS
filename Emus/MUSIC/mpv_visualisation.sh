b74440f5734902ff02f7e28c67feea005b52c9f0
cizia64 <cizia64@gmail.com>
2024-07-26 18:35:58 +0200

    Fix mpv video/music freezes
    - Fix mpv freeze thank to flgr advice: add "--terminal=no"   (issue: https://github.com/cizia64/CrossMix-OS/issues/102)
    - Add few TV channels as video samples (from: https://github.com/Free-TV/IPTV), to use it:
                  - Go in emulators list, press Menu button and select "Edit"
                  - Select "VIDEOS" (located at the end of the emulators list)
                  - Then go in VIDEOS rom list and launch "Show-Hide TV channels"
    
    Co-Authored-By: Florian Gross <florian.s.gross@web.de>

--- Emus/MUSIC/mpv_visualisation.sh
+++ Emus/MUSIC/mpv_visualisation.sh
@@ -8,10 +8,10 @@ cd "$progdir"
 export LD_LIBRARY_PATH=/lib:/lib64:/usr/lib:/mnt/SDCARD/System/lib/
 
 /mnt/SDCARD/Apps/PortMaster/PortMaster/gptokeyb2 -1 "mpv" -c "keys.gptk" &
-sleep 1
+sleep 0.4
 
-echo 1 > /tmp/stay_awake
-HOME="$progdir" /mnt/SDCARD/System/bin/mpv "$@" --fullscreen --audio-buffer=1 --lavfi-complex="[aid1]asplit[ao][a]; [a]showcqt[vo]" #--script=/mnt/SDCARD/Emus/VIDEOS/.config/mpv/metadata_osd.lua  #--autofit=100%x1280    # for music: --geometry=720
+echo 1 >/tmp/stay_awake
+HOME="$progdir" /mnt/SDCARD/System/bin/mpv "$@" --fullscreen --audio-buffer=1 --cache=no --terminal=no --lavfi-complex="[aid1]asplit[ao][a]; [a]showcqt[vo]" #--script=/mnt/SDCARD/Emus/VIDEOS/.config/mpv/metadata_osd.lua  #--autofit=100%x1280    # for music: --geometry=720
 rm /tmp/stay_awake
 
 pkill -9 gptokeyb2
