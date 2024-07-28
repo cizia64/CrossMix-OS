ea030763e3e6fb76d308d370b97d3409252ad1b4
nevrdid <nevrdid@msn.com>
2024-07-28 04:01:56 +0200

    Added Advance MAME as a new system
    Configuration is probably not optimized but it is working well like
    that.
    Thanks to Trngaje, i get "all" controls to work.
    One issue however is that we can't set vertical controls for ui so we
    must use horizontal ones.
    Keybinds:
        select to insert coin
        start to start game
        d-pad for ui directions
        menu to acces configuration
        start+select to acces main menu
        r2 to show speed
        l2 to pause
        a, b, x, y, l1, r2 for butons 1 to 6
        Horizontal directions: d-pad or left stick for directions
        Vertical directions: d-pad or right stick for directions
    
    References:
    - AdvMame version 3.10 standalone (https://github.com/amadvance/advancemame)
    - Onion advmame.rc (https://github.com/OnionUI/Onion/blob/main/static/configs/BIOS/.advance/advmame.rc)
    - Advance mame logo (https://github.com/ben0bi/esLIEB)

---
+++ Emus/ADVMAME/advmame.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
+cpufreq.sh ondemand 4 7
+
+export LD_LIBRARY_PATH="$PWD/lib:/mnt/SDCARD/System/lib:/usr/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+
+Gamedir=$(dirname "$@")
+Gamefile=$(basename "$@")
+HOME="$PWD" ./advmame -dir_rom "$Gamedir" "${Gamefile%.*}"
