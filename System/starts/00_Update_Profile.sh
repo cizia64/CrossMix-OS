18c5493c15041fe7ee7328334e8e707a79811b5a
nevrdid <nevrdid@msn.com>
2024-08-01 16:03:23 +0200

    Added all paths

--- System/starts/00_Update_Profile.sh
+++ System/starts/00_Update_Profile.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 if ! grep -q "/mnt/SDCARD/System/bin" /etc/profile; then
-    echo 'export PATH="/mnt/SDCARD/System/bin${PATH:+:$PATH}"' >>/etc/profile
+    echo 'export PATH="/mnt/SDCARD/System/usr/trimui/scripts:/mnt/SDCARD/System/bin:/bin:/usr/bin:/usr/trimui/bin"' >>/etc/profile
 fi
 if ! grep -q "/mnt/SDCARD/System/lib" /etc/profile; then
-    echo 'export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"' >>/etc/profile
+    echo 'export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/lib:/usr/lib:/usr/trimui/lib"' >>/etc/profile
 fi
 
