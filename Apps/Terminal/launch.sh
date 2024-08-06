37a0fc5b7e1d039c58562242e55a72c86130c764
nevrdid <nevrdid@msn.com>
2024-08-06 03:20:50 +0200

    Fix typo

--- Apps/Terminal/launch.sh
+++ Apps/Terminal/launch.sh
@@ -3,7 +3,7 @@
 custom_intputd=$(ls -la /usr/trimui/bin | grep "trimui_inputd -> /usr/trimui/bin/trimui_inputd_patched")
 
 if [ -n "$custom_intputd" ]; then
-    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "Patched inputd break the terminal. Pelase switch to original inputd for System Tools." -fs "22" -k "A B START SELECT"
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "Patched inputd break the terminal. Switch back to original inputd to use it." -fs "22" -k "A B START SELECT"
     exit 1
 fi
 
