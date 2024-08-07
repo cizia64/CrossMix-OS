63cdc1e96f68e22143b0cb4b7b3f4d4289b8b508
cizia64 <cizia64@gmail.com>
2024-08-08 01:15:41 +0200

    Update strace
    - Changed save state and load state shortcut keys (to fit with default one: https://github.com/cizia64/CrossMix-OS/wiki/shortcuts#in-game)
    - Changed standalone script name for consistency with other standalones

--- Emus/GBA/mgba_standalone.sh
+++ Emus/GBA/mgba_standalone.sh
@@ -2,7 +2,7 @@
 echo $0 $*
 
 EMU_DIR=/mnt/SDCARD/Emus/GBA
-export LD_LIBRARY_PATH="$EMU_DIR/lib:/usr/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:$EMU_DIR/lib:/usr/lib:$LD_LIBRARY_PATH"
 export XDG_CONFIG_HOME="$EMU_DIR/.config"
 
 $EMU_DIR/cpufreq.sh
@@ -11,4 +11,6 @@ $EMU_DIR/cpufreq.sh
 sleep 0.3
 
 $EMU_DIR/mgba "$@" 
-kill gptokeyb2
+kill -9 $(pidof gptokeyb)
+
+
