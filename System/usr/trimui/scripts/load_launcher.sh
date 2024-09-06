8433b8510d80c6326a93bb7dc9640e253518d14b
nevrdid <nevrdid@msn.com>
2024-09-06 14:41:09 +0200

    - Fix some save issues - Cleaner loader

--- System/usr/trimui/scripts/load_launcher.sh
+++ System/usr/trimui/scripts/load_launcher.sh
@@ -4,10 +4,13 @@ export LD_LIBRARY_PATH="/usr/trimui/lib:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 EMU_DIR="$(dirname "$0")"
 GAME=$(basename "$@")
 
+# Look for a saved preset
 if [ -e "$EMU_DIR/presets.txt" ]; then
+  # Is there a preset for the game ?
 	Launcher_name=$(grep -i "$GAME" "$EMU_DIR/presets.txt" | cut -d'=' -f2)
 
 	if [ -z "$Launcher_name" ]; then
+    # else is there a default preset ?
 		Launcher_name=$(grep -i default "$EMU_DIR/presets.txt" | cut -d'=' -f2)
 	fi
 
@@ -17,15 +20,17 @@ if [ -e "$EMU_DIR/presets.txt" ]; then
 	fi
 fi
 
+# If no preset found
 if [ -z "$Launcher_name" ]; then
+  # Look for the first valid launcher in launchlist
 	if jq -e ".launchlist" "$EMU_DIR/config.json" >/dev/null 2>&1; then
-		# So if it not yet created, search for the first valid launcher.
 		while read launcher; do
 			Launcher_name=$(echo "$launcher" | jq -r '.name')
 			Launcher_command=$(echo "$launcher" | jq -r '.launch')
 			if [ -n "$Launcher_command" ]; then break; fi
 		done < <(jq -c '.launchlist[]' "$EMU_DIR/config.json")
 	else
+    # Else use launch.sh as fallback
 		Launcher_name=Unique
 		Launcher_command="$EMU_DIR/launch.sh"
 	fi
--- System/usr/trimui/scripts/save_launcher.sh
+++ System/usr/trimui/scripts/save_launcher.sh
@@ -4,19 +4,19 @@ GAME=$(basename "$1")
 button_state.sh L
 if [ $? -eq 10 ]; then
 	Launcher_name=$(grep "dowork 0x" "/tmp/log/messages" | tail -n 1 | sed -e 's/.*: \(.*\) dowork 0x.*/\1/')
-	if [ -e "$EMU_DIR/presets.txt" ]; then
-		sed -i "/$GAME/d" "$EMU_DIR/presets.txt"
-	fi
-	echo "$GAME=$Launcher_name" >>"$EMU_DIR/presets.txt"
+	sed -i "/^$GAME/d" presets.txt
+	echo "$GAME=$Launcher_name" >>presets.txt
 else
+
 	# Save launcher as default one
 	button_state.sh R
 	if [ $? -eq 10 ]; then
 		Launcher_name=$(grep "dowork 0x" "/tmp/log/messages" | tail -n 1 | sed -e 's/.*: \(.*\) dowork 0x.*/\1/')
-		if [ -e "$EMU_DIR/presets.txt" ]; then
-			sed -i "s/^default=.*$/default=$Launcher_name/" "$EMU_DIR/presets.txt"
+		if [ ! -f presets.txt ]; then
+			touch presets.txt
 		else
-			echo "default=$Launcher_name" >"$EMU_DIR/presets.txt"
+			sed -i "/^default=/d" presets.txt
 		fi
+		sed -i "1idefault=$Launcher_name" presets.txt
 	fi
 fi
