095e9fbd0ab7793718621e365822f8558fa1345b
nevrdid <nevrdid@msn.com>
2024-09-20 21:23:48 +0200

    Fix wrong file selected for retroarch.

--- Emus/FBNEO/FBNEO_vertival.sh
+++ Emus/FBNEO/FBNEO_vertival.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
 RA_DIR=/mnt/SDCARD/RetroArch
-EMU_DIR=/mnt/SDCARD/Emus/ARCADE_FBNEO
+EMU_DIR=/mnt/SDCARD/Emus/FBNEO
 
 $EMU_DIR/cpufreq.sh
 
@@ -12,7 +12,7 @@ TMP_ROM="$TMP_DIR/$Gamename"
 mv "$1" "$TMP_ROM"
 
 cd "$RA_DIR/"
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/fbneo_libretro.so "$1"
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/fbneo_libretro.so "$TMP_ROM"
 
 mv "$TMP_ROM" "$1"
 rmdir "$TMP_DIR"
