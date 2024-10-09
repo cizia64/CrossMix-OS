6c4954909ecdcdc4ea08660e3ab3266f92c96aaf
cobaltgit <cobaltmail4204@gmail.com>
2024-10-09 10:53:22 +0100

    Consistent cpufreq naming

--- Emus/X1/cpufreq.sh
+++ Emus/X1/cpufreq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
