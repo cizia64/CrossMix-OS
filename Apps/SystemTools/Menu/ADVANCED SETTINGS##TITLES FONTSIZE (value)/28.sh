e59b618dcaca6e3a3076c0f3e9e886d9e4e3244a
nevrdid <nevrdid@msn.com>
2024-10-23 15:25:24 +0200

    * Add 32, 36 and default reset sizes * Backup original configs

--- Apps/SystemTools/Menu/ADVANCED SETTINGS##TITLES FONTSIZE (value)/28.sh
+++ Apps/SystemTools/Menu/ADVANCED SETTINGS##TITLES FONTSIZE (value)/28.sh
@@ -1,5 +1,8 @@
 for i in /mnt/SDCARD/Themes/**/config.json; do
-    sed -i 's/"content_font1":[0-9]*/"content_font1":28/' $i
+    if [ ! -f "$i".bck ]; then
+      cp "$i" "$i".bck
+    fi
+    sed -i 's/"content_font1":[0-9]*/"content_font1":28/' "$i"
 done
 
 /mnt/SDCARD/System/usr/trimui/scripts/mainui_state_update.sh "TITLES FONTSIZE" "28"
---
+++ Apps/SystemTools/Menu/ADVANCED SETTINGS##TITLES FONTSIZE (value)/default.sh
@@ -0,0 +1,6 @@
+
+for i in /mnt/SDCARD/Themes/**/config.json.bck; do
+    mv "$i" "$(basename "$i" .bck)"
+done
+
+/mnt/SDCARD/System/usr/trimui/scripts/mainui_state_update.sh "TITLES FONTSIZE" "default"
