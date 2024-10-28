6965b6d23cffb1da184fd94676c22935960b635a
nevrdid <nevrdid@msn.com>
2024-10-28 18:55:28 +0100

    Fixed games slow because of oss sound driver

--- Emus/ADVMAME/advmame.sh
+++ Emus/ADVMAME/advmame.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
-cpufreq.sh ondemand 4 7
+cpufreq.sh ondemand 2 6
 
 export LD_LIBRARY_PATH="$PWD/lib:/mnt/SDCARD/System/lib:/usr/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 Gamedir=$(dirname "$@")
 Gamefile=$(basename "$@")
-HOME="$PWD" ./advmame4 -dir_rom "$Gamedir" "${Gamefile%.*}"
+HOME="$PWD" ./advmame -dir_rom "$Gamedir" "${Gamefile%.*}"
--- Emus/ADVMAME/advmame310.sh
+++ Emus/ADVMAME/advmame310.sh
@@ -1,7 +1,6 @@
-
 #!/bin/sh
 source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
-cpufreq.sh ondemand 4 7
+cpufreq.sh ondemand 2 6
 
 export LD_LIBRARY_PATH="$PWD/lib:/mnt/SDCARD/System/lib:/usr/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
