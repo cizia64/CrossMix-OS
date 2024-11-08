b72b4dd1edec6187fbd2c6d69eb10e3a5c904a54
nevrdid <nevrdid@msn.com>
2024-11-08 19:26:22 +0100

    Fix missing new gptokeyb2 dependency for mgba standalone

--- Emus/GBA/mgba_standalone.sh
+++ Emus/GBA/mgba_standalone.sh
@@ -2,7 +2,7 @@
 source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
 cpufreq.sh ondemand 2 6
 
-export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:$EMU_DIR/lib:/usr/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$PM_DIR:/mnt/SDCARD/System/lib:$EMU_DIR/lib:/usr/lib:$LD_LIBRARY_PATH"
 export XDG_CONFIG_HOME="$EMU_DIR/.config"
 
 /mnt/SDCARD/Apps/PortMaster/PortMaster/gptokeyb2 "mgba" -k "mgba" -c "/mnt/SDCARD/Emus/GBA/.config/mgba/mgba.gptk" &
