9afe9106d06fd51051fe75949ffeb31b620ccdf7
cizia64 <cizia64@gmail.com>
2024-11-19 23:32:01 +0100

    [Emus] Add Mame 20003 Xtreme core (#252)
    - Add Mame20003 Xtreme
    - Enable OST by default on Mame 2003 Plus
    - Remove old info file

--- Emus/MAME2003PLUS/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
-cpufreq.sh ondemand 2 6
-
-cd $RA_DIR/
-
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/mame2003_plus_libretro.so "$@"
