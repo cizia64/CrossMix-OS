508b877bdb6503bec05902a1f36dd1034f8a2898
pthalin <patrik@thalin.se>
2024-11-20 06:09:26 +0100

    Fix indentation of .sh files

--- Emus/MSX2/openmsx_dsk_error.sh
+++ Emus/MSX2/openmsx_dsk_error.sh
@@ -1,7 +1,9 @@
- /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh \
-    -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/background.png" \
-    -c "0,0,0" \
-    -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
-    -m "C-Bios does not supppor .dsk files. Please use MSX-Bios." \
-    -fs 40 \
-    -k " " 
+#!/bin/sh
+
+/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh \
+   -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/background.png" \
+   -c "0,0,0" \
+   -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
+   -m "C-Bios does not supppor .dsk files. Please use MSX-Bios." \
+   -fs 40 \
+   -k " "
--- Emus/MSX2/openmsx_support.sh
+++ Emus/MSX2/openmsx_support.sh
@@ -1,7 +1,9 @@
- /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh \
-     -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/guide.png" \
-     -c "0,0,0" \
-     -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
-     -m "" \
-     -fs 50 \
-     -k " "
+#!/bin/sh
+
+/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh \
+    -i "/mnt/SDCARD/Emus/MSX2/openmsx/resources/guide.png" \
+    -c "0,0,0" \
+    -f "/mnt/SDCARD/System/resources/DejaVuSans.ttf" \
+    -m "" \
+    -fs 50 \
+    -k " "
