b5db990180cd353f96054c87a54348ad5371c7eb
cizia64 <cizia64@gmail.com>
2024-11-24 02:21:54 +0100

    Adding radios playlists for GMU
    - Adding radios for GMU
    - Origin : https://github.com/junguler/m3u-radio-music-playlists
    - Based on "radio-trucker" and "keep_one" playlists and reorganized by categories.
    - Created a retrogaming playlist "Gaming Radios" (could be used as MainUI background music)
    
    Usage: Browse the playlists directly from GMU file explorer, use Y to append the selected playlist to GMU current playlist.
    
    Additional information:
    - Reading radios from GMU is considered as an experimental feature: it can hang, freeze... (Menu+Power to force stop the application or power button during 8 seconds in worst case).
    - https/ssl radios are not working.

--- Apps/MusicPlayer/launch.sh
+++ Apps/MusicPlayer/launch.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+export PATH="/mnt/SDCARD/System/bin:/mnt/SDCARD/System/usr/trimui/scripts:$PATH"
+
+# we disable auto play on GMU start from Apps section
 settings_file="/mnt/SDCARD/Apps/MusicPlayer/gmu.settings.conf"
 sed -i 's/^Gmu.AutoPlayOnProgramStart=yes$/Gmu.AutoPlayOnProgramStart=no/' "$settings_file"
+
+first_run_file="/mnt/SDCARD/Apps/MusicPlayer/FirstRunDone"
+radios_archive="/mnt/SDCARD/Apps/MusicPlayer/Radios.7z"
+
+if [ -f "$radios_archive" ]; then
+    infoscreen.sh -m "Extracting radio playlists, please wait..."
+    7zz x -aoa "$radios_archive" -o"/mnt/SDCARD/Roms/MUSIC"
+    mv "$radios_archive" "/mnt/SDCARD/Apps/MusicPlayer/Radios_extracted.7z"
+fi
+
 sync
 
 ./gmu_launcher.sh
