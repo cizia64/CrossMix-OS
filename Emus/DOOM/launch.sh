2c307e4166f9bc444e852a2ff126b5fade547c39
cizia64 <cizia64@gmail.com>
2024-12-01 02:58:00 +0100

    [Emus] Add GZDoom standalone (#263)
    GZDoom v4.13.2
    ----------------------
    GZDoom is a modern port of Doom. It also supports Heretic, Hexen, Strife, Chex Quest, and fan-created games like Harmony and Hacx. On top of that mods are supported too.
    The CrossMix default configuration is inspired from recommended settings from advanced users of the GZDoom community.
    
    A quick sum up
    ---------------------
    - iwad files are main game files. Doom 1, Doom 2, Hexen... each one has its one iwad.
    iwad extensions are generally .wad (😅) and must be placed in /mnt/SDCARD/Roms/DOOM/GZDoom
    - mod /patch files can be wad ,pk3, ipk3, pk7... They modify the behavior, textures, weapons of the previsouly selected iwad
    
    Usage
    --------
    The CrossMix implementation is made to facilitate the use of GZDoom.
    
    Copy your iwads in /mnt/SDCARD/Roms/DOOM/GZDoom
    Copy your mods/patches in /mnt/SDCARD/Roms/DOOM/GZDoom/mods
    
    Go in Emulators -> DOOM -> GZDoom and run the iwad you have added (if you don't see it remember to run EmuCleaner app and refresh roms)
    If you have mods available, you'll be prompted by a file selector screen (press B if you don't want to load any mod for your previously selected iwad).
    
    CrossMix includes an iwad checksum checker, it will tell you if the iwad is not recognized (which is not a good sign for using it with GZDoom) and will give you the current version of your iwad if it is recognized.
    Credits: https://github.com/Ch0wW/wadverifier  for the iwad checksums database et son article de blog:
    https://blog.ch0ww.fr/iwadverifier/
    
    How to find iwads
    ------------------------
    The list of supported iwad/games for GZDoom is located here: https://zdoom.org/wiki/IWAD
    Most of the games are licenced games, so it requires to add your own files (these old Doom games are generally available on all game online platforms for few dollars).
    Note that free shareware versions exist and give a very good first experience of the capacities of GZDoom.
    A pack of the Free-to-play games listed on zdoom wiki is available here (just extract it on sd card root):
    https://drive.google.com/file/d/1zk4h7ommRgY08dI31rBkpdN4O3EC5pps/view?usp=drive_link
    
    How to find mods
    ------------------------
    There is a list on the official zdoom wiki:
    https://zdoom.org/wiki/Category:WADs_created_for_ZDoom
    
    DoomLauncher is a launcher for Windows which allows to download mods, and test it with GZDoom for Windows directly. It's a good way to test the compatibility of your mod like this before moving it your handheld device.
    https://github.com/nstlaurent/DoomLauncher

--- Emus/DOOM/launch.sh
+++ Emus/DOOM/launch.sh
@@ -1,7 +1,69 @@
 #!/bin/sh
-source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
+export LD_LIBRARY_PATH=./lib:/mnt/SDCARD/System/lib:$LD_LIBRARY_PATH
+export PATH="/mnt/SDCARD/System/bin:/mnt/SDCARD/System/usr/trimui/scripts:$PATH"
+
 cpufreq.sh ondemand 2 6
 
-cd $RA_DIR/
+RomPath=$(dirname "$1")
+RomDir=$(basename "$RomPath")
 
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/prboom_libretro.so "$@"
+if [ $RomDir = GZDoom ]; then
+
+	IwadFile=$1
+	IwadName="$(basename "$IwadFile" | sed 's/\.[^.]*$//')"
+
+	echo "***************************************************************************"
+	echo "IwadFile     $IwadFile"
+	echo "RomPath      $RomPath"
+	echo "RomDir       $RomDir"
+	echo "***************************************************************************"
+
+	MODS_DIR="/mnt/SDCARD/Roms/DOOM/GZDoom/mods"
+	GZDOOM_DIR="/mnt/SDCARD/Emus/DOOM/GZDoom"
+	cd "$GZDOOM_DIR"
+
+	# We try to identify the current iwad, if not found in database it will probably not work with GZDoom
+	md5_hash=$(md5sum "$1" | awk '{ print $1 }')
+	echo "md5_hash: $md5_hash"
+
+	version=$(jq -r --arg md5 "$md5_hash" 'to_entries | map(.value[] | select(.MD5Hash == $md5) | .Version) | .[0]' "$GZDOOM_DIR/iwad_checksums.json")
+
+	if [ "$version" == "null" ] || [ -z "$version" ]; then
+		echo "Error: No matching version found for the selected IWAD file."
+		infoscreen.sh -i "$GZDOOM_DIR/bg_doom.png" -m "$(basename "$IwadFile") is an unknown iwad file. (menu+power to exit if frozen)" -t 2
+		version="$IwadName (Unknown)"
+	else
+		echo "Iwad Version: $version"
+	fi
+	echo "***************************************************************************"
+
+	# if additional mod files are present (wad,pk3,ipk3,pk7) , show a selector
+	if [ "$(find "$MODS_DIR" -type f 2>/dev/null)" ]; then
+		selector_output=$(selector -t "Choose a mod file to load, or press B to continue without a mod." -d "$MODS_DIR")
+		selected_file="${selector_output#*: }"
+		echo "Selected file: $selected_file"
+	fi
+
+	touch /var/trimui_inputd/swap_ab
+	sync
+
+	# Check if a file was selected or if "No file selected" was returned
+	if ! [ -f "$selected_file" ]; then
+		echo "No additional file selected or file not found. Launching gzdoom with only the iwad."
+		infoscreen.sh -i "$GZDOOM_DIR/bg_doom.png" -m "$version"
+		cd "$GZDOOM_DIR"
+		HOME="$GZDOOM_DIR" ./gzdoom -iwad "$IwadFile"
+	else
+		infoscreen.sh -i "$GZDOOM_DIR/bg_doom.png" -m "$version + $(basename "$selected_file" | sed 's/\.[^.]*$//')"
+		cd "$GZDOOM_DIR"
+		HOME="$GZDOOM_DIR" ./gzdoom -iwad "$IwadFile" -file "$selected_file"
+	fi
+
+	rm /var/trimui_inputd/swap_ab
+	sync
+else
+	source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
+	cd $RA_DIR/
+
+	HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/prboom_libretro.so "$@"
+fi
