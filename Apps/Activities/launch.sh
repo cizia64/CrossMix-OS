c27a15e99e188bb8f673989116a77e0dfcd0843a
nevrdid <nevrdid@msn.com>
2024-12-07 13:34:39 +0100

    Add theme change on themepack change

--- Apps/Activities/launch.sh
+++ Apps/Activities/launch.sh
@@ -4,10 +4,10 @@ echo $0 $*
 cd "$(dirname "$0")"
 
 display=$(fbset | grep ^mode | cut -d "\"" -f 2)
-if [ "$display" == "1280x720-64" ]
+if [ "$display" = "1280x720-64" ]; then
     sed -iE 's/^device=.*$/device=tsp' data/config.ini
 else
     sed -iE 's/^device=.*$/device=brick' data/config.ini
-end
+fi
 
 /mnt/SDCARD/System/bin/activities gui
