c5466b363b54c66295a629f1c1fc5f15dd1353bf
nevrdid <nevrdid@msn.com>
2024-12-07 12:16:32 +0100

    Appy themes change with systools scripts

--- Apps/SystemTools/Menu/ADVANCED SETTINGS##THEMES (value)/Default.sh
+++ Apps/SystemTools/Menu/ADVANCED SETTINGS##THEMES (value)/Default.sh
@@ -6,6 +6,7 @@ theme=$(basename "$0" .sh)
 if [ -d "/mnt/SDCARD/Themes/${theme}" ]; then
     /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "Applying \"${theme}\" theme."
     /usr/trimui/bin/systemval "theme" "/mnt/SDCARD/Themes/${theme}/"
+    sed -iE 's/^skins_theme=.*$/skins_theme='"$theme" /mnt/SDCARD/Apps/Activities/data/config.ini
 else
     echo "Theme directory Themes/${theme} does not exist."
     /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "\"${theme}\" theme directory does not exist !!" -c red -t 3
