ae58632bf04c849293d026965c2cbd8b6fbba69f
nevrdid <nevrdid@msn.com>
2024-12-07 12:26:17 +0100

    check device from launcher

--- Apps/Activities/launch.sh
+++ Apps/Activities/launch.sh
@@ -2,4 +2,12 @@
 echo $0 $*
 
 cd "$(dirname "$0")"
+
+display=$(fbset | grep ^mode | cut -d "\"" -f 2)
+if [ "$display" == "1280x720-64" ]
+    sed -iE 's/^device=.*$/device=tsp' data/config.ini
+else
+    sed -iE 's/^device=.*$/device=brick' data/config.ini
+end
+
 /mnt/SDCARD/System/bin/activities gui
