4bc1fa3618444ce82c1a25760f5f235473dbcc9f
cizia64 <cizia64@gmail.com>
2024-12-07 23:48:27 +0100

    MENU + START exit shortcut for PICO-8 Wrapper

--- Emus/PICO/Pico8 Wrapper - Splore.sh
+++ Emus/PICO/Pico8 Wrapper - Splore.sh
@@ -11,7 +11,6 @@ export LD_LIBRARY_PATH="$picodir/lib:/usr/lib:$LD_LIBRARY_PATH"
 
 if ! [ -f /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/bin/pico8_64 ] || ! [ -f /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/bin/pico8.dat ]; then
 	LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/usr/trimui/lib:$LD_LIBRARY_PATH"
-
 	/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "To use PICO-8 Wrapper, you need purchased PICO-8 binaries (add pico8_64 and pico8.dat)." -fs 25 -t 5
 	exit
 else
@@ -22,6 +21,11 @@ else
 	fi
 fi
 
+# To support MENU + START exit shortcut
+/mnt/SDCARD/System/bin/thd --triggers /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/cfg/thd.conf /dev/input/event3 &
+
 mount --bind /mnt/SDCARD/Roms/PICO /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/.lexaloffle/pico-8/carts
 pico8_64 -splore -preblit_scale 3
+
+kill -9 $(pidof thd)
 umount /mnt/SDCARD/Apps/pico/.lexaloffle/pico-8/carts
--- Emus/PICO/Pico8 Wrapper.sh
+++ Emus/PICO/Pico8 Wrapper.sh
@@ -14,6 +14,12 @@ if ! [ -f /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/bin/pico8_64 ] || ! [ -f /mnt/SDCA
 	/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "To use the official PICO-8, you need to add your purchased binaries (pico8_64 and pico8.dat)." -fs 25 -t 5
 fi
 
+# To support MENU + START exit shortcut
+/mnt/SDCARD/System/bin/thd --triggers /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/cfg/thd.conf /dev/input/event3 &
+
+
 mount --bind /mnt/SDCARD/Roms/PICO8 /mnt/SDCARD/Emus/PICO/PICO8_Wrapper/.lexaloffle/pico-8/carts
 pico8_64 -preblit_scale 3 -run "$1"
+
+kill -9 $(pidof thd)
 umount /mnt/SDCARD/Apps/pico/.lexaloffle/pico-8/carts
