783a86a8bfca9a43667999761b2e1a29d7db9676
Nevrdid <nevrdid@msn.com>
2024-12-08 19:04:12 +0100

    Launchlists changes (#282)
    Renamed some launcher for easier identification

--- Emus/PICO/retro8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
-cpufreq.sh ondemand 3 6
-
-cd $RA_DIR/
-
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/retro8_libretro.so "$@"
