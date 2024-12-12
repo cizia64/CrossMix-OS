d53f7cd1ed1d8e612cd6876a85b2efef0ac21783
cizia64 <cizia64@gmail.com>
2024-12-12 23:26:43 +0100

    Update launch.sh

--- Apps/OTA-update/launch.sh
+++ Apps/OTA-update/launch.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/usr/trimui/lib:/mnt/SDCARD/Apps/PortMaster/PortMaster:$LD_LIBRARY_PATH"
 
+echo 1 >/tmp/stay_awake
 /mnt/SDCARD/Apps/PortMaster/PortMaster/gptokeyb2 -1 "Terminal" -c "/mnt/SDCARD/Apps/OTA-update/keys.gptk" &
 /mnt/SDCARD/Apps/Terminal/launch.sh -e "/mnt/SDCARD/System/usr/trimui/scripts/ota_update.sh"
 
 kill -9 $(pidof gptokeyb2)
+rm /tmp/stay_awake
