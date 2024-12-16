d9ec8f6844cc84919d61a4ca4abc99389694a519
nevrdid <nevrdid@msn.com>
2024-12-16 02:22:11 +0100

    Added screen multiplexer to have more possibilities and fix some programs incompatibilities

--- Apps/Terminal/launch.sh
+++ Apps/Terminal/launch.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 progdir=$(dirname "$0")
-LD_LIBRARY_PATH="$progdir/lib:$LD_LIBRARY_PATH"
-res_dir=$progdir/resources
-
+export LD_LIBRARY_PATH="$progdir/lib:$LD_LIBRARY_PATH"
 display=$(fbset | grep ^mode | cut -d "\"" -f 2)
 [ "$display" != "1280x720-64" ] && BRICK_FLAG="-d brick"
-
-$progdir/TermSP -k $BRICK_FLAG -f "$res_dir/Hack-Regular.ttf" -b "$res_dir/Hack-Bold.ttf" -s22 -e "sh" "$@"
+$progdir/TermSP -k $BRICK_FLAG -e "$progdir/screen" -c "$progdir/.screenrc" "$@"
