611742be7c1965d8c5a0f6d1eac965c916371c71
cizia64 <cizia64@gmail.com>
2024-12-22 02:26:24 +0100

    Update crossmix_update.sh
    Override colors during updates as we don't use a Terminal app which support it for now

--- System/usr/trimui/scripts/crossmix_update.sh
+++ System/usr/trimui/scripts/crossmix_update.sh
@@ -11,6 +11,12 @@ source /mnt/SDCARD/System/usr/trimui/scripts/update_common.sh
 echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 1608000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
+RED=""
+GREEN=""
+YELLOW=""
+BLUE=""
+PURPLE=""
+NC=""
 
 main() {
 # Find the update file
