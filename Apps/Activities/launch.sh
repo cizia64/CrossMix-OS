312308f82e2e74308dc383759ed41363750d54c1
nevrdid <nevrdid@msn.com>
2024-12-30 14:59:28 +0100

    Adapt device recognition

--- Apps/Activities/launch.sh
+++ Apps/Activities/launch.sh
@@ -2,12 +2,7 @@
 echo $0 $*
 
 cd "$(dirname "$0")"
-
-display=$(fbset | grep ^mode | cut -d "\"" -f 2)
-if [ "$display" = "1280x720-64" ]; then
-    sed -iE 's/^device=.*$/device=tsp' data/config.ini
-else
-    sed -iE 's/^device=.*$/device=brick' data/config.ini
-fi
+read -r device < /etc/trimui_device.txt
+sed -iE 's/^device=.*$/device='"$device/" data/config.ini
 
 /mnt/SDCARD/System/bin/activities gui
