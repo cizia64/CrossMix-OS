ab43e7cfc2385ed8c4e9410656b1234b3a1d8e71
nevrdid <nevrdid@msn.com>
2025-01-02 23:28:04 +0100

    Adapt to device resolution automaticly

--- Apps/Terminal/launch.sh
+++ Apps/Terminal/launch.sh
@@ -2,6 +2,4 @@
 
 progdir=$(dirname "$0")
 export LD_LIBRARY_PATH="$progdir/lib:$LD_LIBRARY_PATH"
-display=$(fbset | grep ^mode | cut -d "\"" -f 2)
-[ "$display" != "1280x720-64" ] && BRICK_FLAG="-d brick"
-$progdir/TermSP -k $BRICK_FLAG -e "$progdir/screen" -c "$progdir/.screenrc" "$@"
+$progdir/TermSP -k -e "$progdir/screen" -c "$progdir/.screenrc" "$@"
