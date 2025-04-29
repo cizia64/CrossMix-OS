de1b4b8769d54f84450900d9380c461242c389a5
Italo Maia <italo.maia@gmail.com>
2025-04-30 00:24:25 +0200

    fix: using full path to lcd_on|off
    Per MR review: relative path would fail because the context is different.

--- System/usr/trimui/scripts/lcd_toggle.sh
+++ System/usr/trimui/scripts/lcd_toggle.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # File to store the LCD state
-STATE_FILE="/tmp/lcd_state"
+STATE_FILE=/tmp/lcd_state
+DIR=/mnt/SDCARD/System/usr/trimui/scripts
 
 # Check if the file exists; if not, create it with the "on" state
 if [ ! -f "$STATE_FILE" ]; then
@@ -12,7 +13,7 @@ fi
 CURRENT_STATE=$(cat "$STATE_FILE")
 
 if [ "$CURRENT_STATE" = "off" ]; then
-  ./lcd_on.sh
+  . "$DIR/lcd_on.sh"
 else
-  ./lcd_off.sh
+  . "$DIR/lcd_off.sh"
 fi
