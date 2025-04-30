270d9b6c9420fd8587ca4444871a8fbc0c0d5def
Italo Maia <italo.maia@gmail.com>
2025-04-30 02:22:10 +0200

    fix: install uses absolute path for files
    The relative path depends on where the script is and we don't want to worry about that. DIR is now fetched dynamically.

--- trimui/fn_editor/turn_led_on/install.sh
+++ trimui/fn_editor/turn_led_on/install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-fn_script=com.trimui.led_on.sh
+fn_script_fn=com.trimui.led_on.sh
 fn_editor_fd=/usr/trimui/apps/fn_editor
 fn_editor_scripts="$fn_editor_fd/scripts.json"
 fn_editor_tmp=/tmp/fn_editor.json
 
 # we need `jq`
 PATH="/mnt/SDCARD/System/bin:$PATH"
+DIR=`dirname $0`
 
-if (grep -q $fn_script $fn_editor_scripts 2>/dev/null); then
-    echo "$fn_script is already installed; nothing to do."
+if (grep -q $fn_script_fn $fn_editor_scripts 2>/dev/null); then
+    echo "$fn_script_fn is already installed; nothing to do."
 else
-    cfg_js=`cat ./config.json`
+    cfg_js=`cat $DIR/config.json`
 
-    cp $fn_script "$fn_editor_fd/"
-    echo `cat $fn_editor_scripts` | jq ".[.[]|length+1] += $cfg_js" > $fn_editor_tmp 
+    cp "$DIR/$fn_script_fn" "$fn_editor_fd/"
+    echo `cat $fn_editor_scripts` | jq ".[.[]|length+1] += $cfg_js" > $fn_editor_tmp
     mv -f $fn_editor_tmp $fn_editor_scripts
 fi
