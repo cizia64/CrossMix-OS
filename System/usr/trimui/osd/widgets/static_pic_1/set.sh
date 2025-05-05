a288378834c3cf91403f41e5104dd1bc32fb1bcf
cizia64 <cizia64@gmail.com>
2025-05-05 21:58:33 +0200

    Add Night mode shortcut  configuration in System Tools
    - Add Night mode shortcut  configuration in System Tools
    - Re-apply Fn night mode when exiting game to MainUI
    - Fix useless sound on image

--- System/usr/trimui/osd/widgets/static_pic_1/set.sh
+++ System/usr/trimui/osd/widgets/static_pic_1/set.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "click:"$1
-if [ -f /tmp/trimui_osd/osdd_show_u2 ]; then
-	aplay $(pwd)/widgets/static_pic_1/abe_fart.wav &
+if [ -f /tmp/trimui_osd/osdd_show_u2 ] ; then
+	LD_LIBRARY_PATH=/usr/trimui/lib
+	/usr/trimui/bin/mplayer -ao alsa -format s16le -novideo -softvol -softvol-max 100 -volume 100  /mnt/SDCARD/System/usr/trimui/osd/widgets/static_pic_1/abe_fart.wav &
 fi
