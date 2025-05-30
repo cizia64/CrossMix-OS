fbf4096be47c99a26f99ff95f187b4eae09d8fea
Osama A.M. <71850746+udbue@users.noreply.github.com>
2025-05-30 05:59:30 +0000

    Delete Emus/SEGA32X/genplus_wide.sh

--- Emus/SEGA32X/genplus_wide.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
-cpufreq.sh ondemand 2 6
-
-/mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "/mnt/SDCARD/RetroArch/.retroarch/config/Genesis Plus GX Wide/MD.cfg"  "input_overlay_enable" "false"
-
-cd $RA_DIR/
-
-HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/genesis_plus_gx_wide_libretro.so "$@"
