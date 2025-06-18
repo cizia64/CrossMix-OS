17eea5288710e0cc736a8a8a8901355ee0131caf
cizia64 <cizia64@gmail.com>
2025-06-18 23:12:22 +0200

    New playlist generator + new presenter app
    - More efficient PlayList generator script in Python (faster and more efficient on special file names like [disc x] or [SLES-xxxxx]
    - New "presenter" app based on minui-presenter by Jose Diaz-Gonzalez (allows multiline messages)
    Official repo: https://github.com/josegonzalez/minui-presenter
    Forked repo: https://github.com/cizia64/minui-presenter
    
    Co-Authored-By: Jose Diaz-Gonzalez <email@josediazgonzalez.com>

--- Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
+++ Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
@@ -1,176 +1,45 @@
-# Credits: gotbletu (@gmail|twitter|youtube|github|lbry)
-#          adapted by Onion Team (https://github.com/OnionUI/Onion)
+#!/bin/sh
 
-count_m3u="/tmp/count_m3u"
-
-# target files extension
-EXT_INT="cue|gdi|chd|pbp|iso|dsk"
-
-helpmsg() {
-  echo "desc: create m3u playlist for multi disc games [$EXT_INT]"
-  echo "    e.g Playstation, Sega CD/Mega CD, NeoGeo CD,"
-  echo "    PC Engine CD/PC-FX, Amiga"
-  echo ""
-  echo "usage: ${0##*/} [options] [folder name]"
-  echo ""
-  echo "options:"
-  echo "  -md  create multiple directories : one directory and one m3u playlist for each game"
-  echo "      from"
-  echo "          Novastorm (USA) (Disc 1).bin"
-  echo "          Novastorm (USA) (Disc 1).cue"
-  echo "          Novastorm (USA) (Disc 2).bin"
-  echo "          Novastorm (USA) (Disc 2).cue"
-  echo "          Metal Gear Solid (USA) (Disc 1) (Rev 1).chd"
-  echo "          Metal Gear Solid (USA) (Disc 2) (Rev 1).chd"
-  echo "      to"
-  echo "          Novastorm (USA).m3u"
-  echo "          Metal Gear Solid (USA) (Rev 1).m3u"
-  echo "          /.Novastorm (USA)/Novastorm (USA) (Disc 1).bin"
-  echo "          /.Novastorm (USA)/Novastorm (USA) (Disc 1).cue"
-  echo "          /.Novastorm (USA)/Novastorm (USA) (Disc 2).bin"
-  echo "          /.Novastorm (USA)/Novastorm (USA) (Disc 2).cue"
-  echo "          /.Metal Gear Solid (USA) (Rev 1)/Metal Gear Solid (USA) (Disc 1) (Rev 1).chd"
-  echo "          /.Metal Gear Solid (USA) (Rev 1)/Metal Gear Solid (USA) (Disc 2) (Rev 1).chd"
-  echo ""
-  echo "  -sd create a single \"multi-disc\" directory for all multidisk files"
-  echo "      from"
-  echo "          Heart of Darkness (USA) (Disc 1).bin"
-  echo "          Heart of Darkness (USA) (Disc 1).cue"
-  echo "          Heart of Darkness (USA) (Disc 2).bin"
-  echo "          Heart of Darkness (USA) (Disc 2).cue"
-  echo "          Lunar 2 - Eternal Blue Complete (USA) (Disc 1).chd"
-  echo "          Lunar 2 - Eternal Blue Complete (USA) (Disc 2).chd"
-  echo "          Lunar 2 - Eternal Blue Complete (USA) (Disc 3).chd"
-  echo "      to"
-  echo "          Heart of Darkness (USA).m3u"
-  echo "          Lunar 2 - Eternal Blue Complete (USA).m3u"
-  echo "          .multi-disc/Heart of Darkness (USA) (Disc 1).bin"
-  echo "          .multi-disc/Heart of Darkness (USA) (Disc 1).cue"
-  echo "          .multi-disc/Heart of Darkness (USA) (Disc 2).bin"
-  echo "          .multi-disc/Heart of Darkness (USA) (Disc 2).cue"
-  echo "          .multi-disc/Lunar 2 - Eternal Blue Complete (USA) (Disc 1).chd"
-  echo "          .multi-disc/Lunar 2 - Eternal Blue Complete (USA) (Disc 2).chd"
-  echo "          .multi-disc/Lunar 2 - Eternal Blue Complete (USA) (Disc 3).chd"
-  echo ""
-  echo "  -h, --help                 display this help message"
-  echo ""
-  echo "Swap Disc: Retroarch > [Load Your Game] > Quick Menu > Disc Control"
-}
-
-if [ "$1" = -h ] || [ "$1" = --help ]; then
-  helpmsg
-  exit 0
-elif [ "$1" = -md ]; then
-  TARGETFOLDER="-md"
-elif [ "$1" = -sd ]; then
-  TARGETFOLDER="-sd"
-else # case for eventual GLO menu
-  # LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so prompt -t "Multi-disc - Target Directory" -m "Choose the sub folder name where\noriginal disc files will be stored." \
-  # "Single directory (\".multi-disc\")" \
-  # "Multiple directories (\".Game_Name\")"
-  # retcode=$?
-
-  retcode=1
-
-  if [ $retcode -eq 0 ]; then
-    TARGETFOLDER="-sd"
-  elif [ $retcode -eq 1 ]; then
-    TARGETFOLDER="-md"
-  elif [ $retcode -eq 255 ]; then
-    exit
-  fi
-fi
-
-echo 0 >"$count_m3u"
-# create missing cue files.
-"/mnt/SDCARD/Apps/SystemTools/Menu/TOOLS/CUE - Generator.sh"
-
-if [ "$TARGETFOLDER" = -sd ]; then
-  DIR_NAME=".multi-disc"
-fi
+export LD_LIBRARY_PATH=/mnt/SDCARD/Apps/TubeExJuk:/mnt/SDCARD/System/lib:/lib64:/usr/trimui/lib:/usr/lib
+export PATH=$PATH:/mnt/SDCARD/System/bin
 
 DIR_LIST="PS SEGACD NEOCD PCE PCFX AMIGA"
+TOTAL=0
+Message=""
+
 for dir in $DIR_LIST; do
-  full_path="/mnt/SDCARD/Roms/$dir"
-  echo "============================= $full_path ============================="
-  if [ -d "$full_path" ]; then
-    cd "$full_path"
-    ### TYPE 1: TitleOfGame (USA) (Disc 1).chd
-    #---------------------------------------------------------------------------------
-    find . -maxdepth 1 ! -iname '*.m3u' -type f -iname "*([Dd][Ii][Ss][KkCc] 1).*[$EXT_INT]" | while read line; do
-      # searching for a "Disc 1" file.
-      FILE_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@ ([Dd][Ii][Ss][KkCc] 1)@@g')"
-      if [ "$TARGETFOLDER" = -md ]; then
-        DIR_NAME=".$(echo "${line%.*}" | sed 's@./@@g' | sed 's@ ([Dd][Ii][Ss][KkCc] 1)@@g')"
-        DIR_NAME="${DIR_NAME%"${DIR_NAME##*[![:space:]]}"}" # remove spaces at the end
-      fi
-      SEARCH_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@([Dd][Ii][Ss][KkCc] 1)@([Dd][Ii][Ss][KkCc] ?)@g')"
-      mkdir -p "$DIR_NAME"
-      echo -e "****************\nType 1: ${line}\n**** SEARCH_NAME : $SEARCH_NAME \n**** DIR_NAME : $DIR_NAME\n**** FILE_NAME : $FILE_NAME"
-      count=$(cat "$count_m3u")
-      count=$((count + 1))
-      echo "$count" >"$count_m3u"
-      sync
-      # move matching files to directory
-      find . -maxdepth 1 ! -iname '*.m3u' -type f -iname "$SEARCH_NAME.*" -exec mv -n -- '{}' "$DIR_NAME" \;
-      # Create m3u playslist file
-      find "$DIR_NAME" ! -iname '*.m3u' -type f -iname "$SEARCH_NAME*.*[$EXT_INT]" | sed -e 's/^//' | sort >"$FILE_NAME".m3u
-    done
+    /mnt/SDCARD/System/bin/presenter \
+        --message "$Message\n$dir playlist(s) creation..." \
+        --font-default "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" \
+        --background-image "/mnt/SDCARD/trimui/res/crossmix-os/bg-info.png" \
+        --message-alignment top \
+        --horizontal-alignment left \
+        --preserve-framebuffer \
+        --line-spacing 0 \
+        --show-spinner &
 
-    ### TYPE 2: AnotherTitleOfGame (USA) (Disc 1) (Rev 2).chd
-    #---------------------------------------------------------------------------------
-    find . -maxdepth 1 ! -iname '*.m3u' -type f -iname "*([Dd][Ii][Ss][KkCc] 1) *.*[$EXT_INT]" | while read line; do
-      # searching for a "Disc 1" file.
-      FILE_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@ ([Dd][Ii][Ss][KkCc] 1)@@g')"
-      if [ "$TARGETFOLDER" = -md ]; then
-        DIR_NAME=".$(echo "${line%.*}" | sed 's@./@@g' | sed 's@ ([Dd][Ii][Ss][KkCc] 1)@@g')"
-        DIR_NAME="${DIR_NAME%"${DIR_NAME##*[![:space:]]}"}" # remove spaces at the end
-      fi
-      SEARCH_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@([Dd][Ii][Ss][KkCc] 1)@([Dd][Ii][Ss][KkCc] ?)@g')"
-      echo -e "****************\nType 2: ${line}\n**** SEARCH_NAME : $SEARCH_NAME \n**** DIR_NAME : $DIR_NAME\n**** FILE_NAME : $FILE_NAME"
-      count=$(cat "$count_m3u")
-      count=$((count + 1))
-      echo "$count" >"$count_m3u"
-      sync
-      mkdir -p "$DIR_NAME"
-      # move matching files to directory
-      find . -maxdepth 1 ! -iname '*.m3u' -type f -iname "$SEARCH_NAME.*" -exec mv -n -- '{}' "$DIR_NAME" \;
-      # Create m3u playslist file
-      find "$DIR_NAME" ! -iname '*.m3u' -type f -iname "$SEARCH_NAME*.*[$EXT_INT]" | sed -e 's/^//' | sort >"$FILE_NAME".m3u
-    done
+    COUNT=$(python3.11 "/mnt/SDCARD/System/usr/trimui/scripts/M3U - Playlist Generator.py" -md "/mnt/SDCARD/Roms/$dir" | grep '^TOTAL_M3U_CREATED=' | cut -d= -f2)
+    sleep 2
+    pkill -9 presenter
+    sleep 0.5
 
-    ### TYPE 3: AmstradMSXTitleOfGame (19xx)(Developer)(Disc 1 of 3).dsk
-    #---------------------------------------------------------------------------------
-    find . -maxdepth 1 ! -iname '*.m3u' -type f -iname "*([Dd][Ii][Ss][KkCc] 1 of ?).*[$EXT_INT]" | while read line; do
-      # searching for a "Disc 1" file.
-      FILE_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@([Dd][Ii][Ss][KkCc] 1 of .*)@@g')"
-      if [ "$TARGETFOLDER" = -md ]; then
-        DIR_NAME=".$(echo "${line%.*}" | sed 's@./@@g' | sed 's@([Dd][Ii][Ss][KkCc] 1 of .*)@@g')"
-        DIR_NAME="${DIR_NAME%"${DIR_NAME##*[![:space:]]}"}" # remove spaces at the end
-      fi
-      SEARCH_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@([Dd][Ii][Ss][KkCc] 1 of@([Dd][Ii][Ss][KkCc] ? of@g')"
-      # SEARCH_NAME="$(echo "${line%.*}" | sed 's@./@@g' | sed 's@([Dd][Ii][Ss][KkCc] 1)@([Dd][Ii][Ss][KkCc] ?)@g')"
-      echo -e "****************\nType 3: ${line}\n**** SEARCH_NAME : $SEARCH_NAME \n**** DIR_NAME : $DIR_NAME\n**** FILE_NAME : $FILE_NAME"
-      count=$(cat "$count_m3u")
-      count=$((count + 1))
-      echo "$count" >"$count_m3u"
-      sync
-      mkdir -p "$DIR_NAME"
-      # move matching files to directory
-      find . -maxdepth 1 ! -iname '*.m3u' -type f -iname "$SEARCH_NAME*.*" -exec mv -n -- '{}' "$DIR_NAME" \;
-      # Create m3u playslist file
-      find "$DIR_NAME" ! -iname '*.m3u' -type f -iname "$SEARCH_NAME*.*[$EXT_INT]" | sed -e 's/^//' | sort >"$FILE_NAME".m3u
-    done
-  else
-    echo "Directory $full_path does not exist."
-  fi
+    # Si COUNT est vide, on met 0
+    COUNT=${COUNT:-0}
+    TOTAL=$((TOTAL + COUNT))
+    Message="$Message\n$dir playlist(s) created: $COUNT"
+
 done
 
-/mnt/SDCARD/System/usr/trimui/scripts/reset_list.sh "PS"
-sync
+Message="$Message\n \n--------------------------------------\nFinished.\n \nTotal playlist(s) created: $TOTAL\n \n--------------------------------------\n \n \n                            Press B to quit"
 
-if [ "$#" -eq 0 ]; then # when launched by GLO menu, without args.
+# Message final
+/mnt/SDCARD/System/bin/presenter \
+    --message "$Message" \
+    --font-default "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" \
+    --background-image "/mnt/SDCARD/trimui/res/crossmix-os/bg-info.png" \
+    --message-alignment top \
+    --horizontal-alignment left \
+    --preserve-framebuffer \
+    --line-spacing 0 --action-show
 
-  /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -m "$(cat "$count_m3u") playlist files created." -t 3
-
-fi
+exit 0
