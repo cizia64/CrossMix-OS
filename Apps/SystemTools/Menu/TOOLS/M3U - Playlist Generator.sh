f0a8c4850e3d6c06041544ff9f7239519ab3d15e
cizia64 <cizia64@gmail.com>
2025-06-19 03:57:50 +0200

    Add infoscreen2.sh
    infoscreen2.sh:
    - Similar syntax to infoscreen.sh
    - Use background and font from the current CrossMix Style by default
    - New presenter app options added
    
    Example:
    infoscreen2.sh -m "test" -p top-left -t 3

--- Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
+++ Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
@@ -8,38 +8,26 @@ TOTAL=0
 Message=""
 
 for dir in $DIR_LIST; do
-    /mnt/SDCARD/System/bin/presenter \
-        --message "$Message\n$dir playlist(s) creation..." \
-        --font-default "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" \
-        --background-image "/mnt/SDCARD/trimui/res/crossmix-os/bg-info.png" \
-        --message-alignment top \
-        --horizontal-alignment left \
-        --preserve-framebuffer \
-        --line-spacing 0 \
-        --show-spinner &
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$Message\n$dir playlist(s) creation..." -fi 0 -p top-left -fb -sp &
 
     COUNT=$(python3.11 "/mnt/SDCARD/System/usr/trimui/scripts/M3U - Playlist Generator.py" -md "/mnt/SDCARD/Roms/$dir" | grep '^TOTAL_M3U_CREATED=' | cut -d= -f2)
     sleep 2
-    pkill -9 presenter
-    sleep 0.5
+    pkill presenter
 
-    # Si COUNT est vide, on met 0
+    # If COUNT is empty, set to 0
     COUNT=${COUNT:-0}
     TOTAL=$((TOTAL + COUNT))
     Message="$Message\n$dir playlist(s) created: $COUNT"
 
 done
 
-Message="$Message\n \n--------------------------------------\nFinished.\n \nTotal playlist(s) created: $TOTAL\n \n--------------------------------------\n \n \n                            Press B to quit"
+# Final message:
+Message="$Message\n"
+Message="$Message\n \n--------------------------------------\n"
+Message="$Message Finished.\n"
+Message="$Message \nTotal playlist(s) created: $TOTAL\n"
+Message="$Message \n--------------------------------------\n \n \n "
+Message="$Message Press B to quit"
 
-# Message final
-/mnt/SDCARD/System/bin/presenter \
-    --message "$Message" \
-    --font-default "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" \
-    --background-image "/mnt/SDCARD/trimui/res/crossmix-os/bg-info.png" \
-    --message-alignment top \
-    --horizontal-alignment left \
-    --preserve-framebuffer \
-    --line-spacing 0 --action-show
 
-exit 0
+/mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$Message" -fi 0 -p top-left
