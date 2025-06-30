d37bab3f559b716a7f0d70fb7f74cce8af4b8711
cizia64 <cizia64@gmail.com>
2025-06-30 17:51:31 +0200

    Update set.sh
    Add exception for gambatte which already have R2 Fast Forward implemented in the core directly.
    
    Not perfect because we could enable One Key Fast Forward before running gambatte but it should be OK in most cases.
    Alternative solution: put One Key Fast Forward in System Tools and enable it at each run of RetroArch with an exception list.

--- System/usr/trimui/res/osd/widgets/toggle_fastforward/set.sh
+++ System/usr/trimui/res/osd/widgets/toggle_fastforward/set.sh
@@ -6,7 +6,15 @@ CMD="/mnt/SDCARD/System/bin/r2_fastforward /dev/input/event3"
 if [ $# -eq 0 ]; then
     mkdir -p "$(dirname "$STATUS_FILE")"
 else
-    # Get PIDs of matching process
+
+    # If gambatte is running, do not enable FF
+    if pgrep -f "gambatte_libretro.so" >/dev/null; then
+        ./show_info_msg_extra_long.sh "One key fast forward not required for this core."
+        echo 0 >"$STATUS_FILE"
+        exit 0
+    fi
+
+    # Check if r2_fastforward is already running
     PIDS=$(pgrep -f "r2_fastforward /dev/input/event3")
 
     if [ -n "$PIDS" ]; then
