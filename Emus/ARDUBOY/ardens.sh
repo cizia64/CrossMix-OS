698d908a8013ad1b13ca0bd8a732aa25b3872dda
cizia64 <cizia64@gmail.com>
2025-07-06 22:51:58 +0200

    Add Ardens core (Arduboy)
    https://docs.libretro.com/library/ardens/
    
    Ardens is now the default Arduboy emulator.

---
+++ Emus/ARDUBOY/ardens.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+source /mnt/SDCARD/System/usr/trimui/scripts/common_launcher.sh
+cpufreq.sh ondemand 2 6
+
+cd $RA_DIR/
+
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v -L $RA_DIR/.retroarch/cores/ardens_libretro.so "$@"
