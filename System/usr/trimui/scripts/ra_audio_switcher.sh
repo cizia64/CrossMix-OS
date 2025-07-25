6c4cc86c5cf0828f2f27d66933ee18628e7e88c4
cizia64 <cizia64@gmail.com>
2025-07-25 12:55:44 +0200

    Audio switcher compatibility for FW v1.1.0

--- System/usr/trimui/scripts/ra_audio_switcher.sh
+++ System/usr/trimui/scripts/ra_audio_switcher.sh
@@ -5,5 +5,23 @@ if [ -f "/tmp/btdev_addr" ]; then
     /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "/mnt/SDCARD/RetroArch/retroarch.cfg" "audio_driver" "alsa"
 else
     echo "Bluetooth not in use: Retroarch audio driver set to oss"
-    /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "/mnt/SDCARD/RetroArch/retroarch.cfg" "audio_driver" "oss"
+    
+    if ! grep -q 'audio_driver *= *"oss"' /mnt/SDCARD/RetroArch/retroarch.cfg; then
+        /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "/mnt/SDCARD/RetroArch/retroarch.cfg" "audio_driver" "oss"
+    fi
+fi
+
+
+if pgrep "mplayer" >/dev/null; then
+    CHOICE=$(/mnt/SDCARD/System/bin/presenter --file /mnt/SDCARD/System/resources/Background_Music_choice.json --confirm-button A --no-wrap)
+    
+    if [ "$CHOICE" = "1" ]; then
+        /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "/mnt/SDCARD/RetroArch/retroarch.cfg" "audio_driver" "alsa"
+        elif [ "$CHOICE" = "2" ]; then
+        pkill -9 musicserver
+        pkill -9 mplayer
+        if ! grep -q 'audio_driver *= *"oss"' /mnt/SDCARD/RetroArch/retroarch.cfg; then
+            /mnt/SDCARD/System/usr/trimui/scripts/set_ra_cfg.sh "/mnt/SDCARD/RetroArch/retroarch.cfg" "audio_driver" "oss"
+        fi
+    fi
 fi
