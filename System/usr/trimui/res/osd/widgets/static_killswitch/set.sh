c6b8504f4bb8f680b68257a8ee8378d66d51b434
cizia64 <cizia64@gmail.com>
2025-08-01 01:13:55 +0200

    Fix OSD when resuming game at boot
    + fix OSD hotkey at boot

--- System/usr/trimui/res/osd/widgets/static_killswitch/set.sh
+++ System/usr/trimui/res/osd/widgets/static_killswitch/set.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 echo "click:"$1
 
+if [ -f /tmp/trimui_osd/osdd_show_up ] ; then
 
-detached()
-{
-/mnt/SDCARD/System/bin/sendkey /dev/input/event3 B 2
-/mnt/SDCARD/System/usr/trimui/scripts/cmd_to_run_killer.sh
-}
+	detached()
+	{
+		touch /tmp/hide_osdd
+		/mnt/SDCARD/System/usr/trimui/scripts/cmd_to_run_killer.sh
+	}
 
-detached &
+	detached &
 
+fi
