8cd1792204f4f3cdfaef80a36fc64d0838b627d9
cizia64 <cizia64@gmail.com>
2025-08-03 04:54:24 +0200

    _FirmwareCheck.sh refactoring + fw v1.1.0
    - Add Firmware v1.1.0
    - New look for firmware update UI, only one screen with more information
    - Diagnostic in case of fail.
    - Updated presenter binary for infoscreen2.sh (less lateral margin, less CPU usage, display help):
       https://github.com/cizia64/minui-presenter

--- System/starts/_FirmwareCheck.sh
+++ System/starts/_FirmwareCheck.sh
@@ -21,8 +21,66 @@ fi
 
 CrossMixFWfile="/mnt/SDCARD/trimui/firmwares/MinFwVersion.txt"
 
+if [ ! -f "$CrossMixFWfile" ]; then
+    echo "Configuration file $CrossMixFWfile not found, skipping firmware check."
+    exit 0
+fi
+
 Current_FW_Revision=$(grep 'DISTRIB_DESCRIPTION' /etc/openwrt_release | cut -d '.' -f 3)
 Required_FW_Revision=$(sed -n '2p' "$CrossMixFWfile")
+
+# Function to check CRC and update message
+check_firmware_crc() {
+    local firmware_path="$1"
+    local firmware_file="$2"
+
+    message="${message}Checking firmware integrity........"
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -sp -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Wait.jpg" &
+
+    # Extract CRC from the 7z archive
+    ARCHIVE_CRC=$(/mnt/SDCARD/System/bin/7zz l "$firmware_path" -slt "$firmware_file" | grep "CRC = " | awk '{print $3}' | tr 'a-f' 'A-F')
+
+    # Calculate CRC of the extracted file
+    EXTRACTED_CRC=$(crc32 "/mnt/SDCARD/$firmware_file" | awk '{print $1}' | tr 'a-f' 'A-F')
+
+    # Compare the CRC values
+    if [ -n "$ARCHIVE_CRC" ] && [ -n "$EXTRACTED_CRC" ] && [ "$ARCHIVE_CRC" = "$EXTRACTED_CRC" ]; then
+        echo "FW CRC check passed: $EXTRACTED_CRC"
+        message="${message}OK"
+        return 0
+    else
+        echo "CRC check failed: Archive CRC = $ARCHIVE_CRC, Extracted CRC = $EXTRACTED_CRC"
+        message="${message}KO"
+        return 1
+    fi
+}
+
+diagnose() {
+    pkill presenter
+    sleep 0.5
+    message="${message}\nChecking filesystem..."
+    /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -sp -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Wait.jpg" &
+    check_filesystem
+    FW_Size=$(/mnt/SDCARD/System/bin/7zz l "$FIRMWARE_PATH" -slt "$FIRMWARE_FILE" | awk ' /^Path = / {found=1; next}  found && /^Size = / { print $3; exit }
+')
+
+    FW_Size_MB=$(($FW_Size / 1024 / 1024))
+    if ! check_available_space "$(($FW_Size_MB + 100))"; then
+        echo "Insufficient space available."
+        message="${message}done\nInsufficient space available.\nYou need at least $(($FW_Size_MB + 100)) MB free on SD Card."
+        sleep 1.5
+        pkill presenter
+        sleep 0.5
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Fail.jpg" -k rout A "Exit"
+    else
+        echo "Sufficient space available."
+        message="${message}done\nSufficient space available.\nYou have enough space to update firmware."
+        sleep 1.5
+        pkill presenter
+        sleep 0.5
+        /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Fail.jpg" -k rout A "Exit"
+    fi
+}
 
 echo "Current FW Revision: $Current_FW_Revision"
 echo "Required FW Revision: $Required_FW_Revision"
@@ -35,12 +93,15 @@ else
 
     if [ "$Current_FW_Revision" -lt "$Required_FW_Revision" ]; then
 
-        pgrep -f /usr/trimui/bin/trimui_inputd >/dev/null || /usr/trimui/bin/trimui_inputd & # we need input
+        source /mnt/SDCARD/System/usr/trimui/scripts/common_functions.sh
 
+        pgrep -f trimui_inputd >/dev/null || { cd "/mnt/SDCARD/trimui/app" && ./trimui_inputd & } # we need input
+
+        CrossMix_version=$(cat /mnt/SDCARD/System/usr/trimui/crossmix-version.txt)
         Current_FW_Version="$(cat /etc/version)"
         Required_FW_Version=$(sed -n '1p' "$CrossMixFWfile")
 
-        Echo "Current firmware ($Current_FW_Version - $Current_FW_Revision) must be updated to $Required_FW_Version - $Required_FW_Revision to support CrossMix OS v$version."
+        echo "Current firmware ($Current_FW_Version - $Current_FW_Revision) must be updated to $Required_FW_Version - $Required_FW_Revision to support CrossMix OS v$CrossMix_version."
 
         # Install new busybox from PortMaster, credits : https://github.com/kloptops/TRIMUI_EX
 
@@ -83,42 +144,89 @@ else
 
         sync
 
-        button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i "/mnt/SDCARD/trimui/firmwares/FW_Informations.png" -m "Current FW version: $Current_FW_Version - $Current_FW_Revision                Required FW version: $Required_FW_Version - $Required_FW_Revision" -fs 28 -k "A")
+        FIRMWARE_PATH="/mnt/SDCARD/trimui/firmwares/firmware_${current_device}_v${Required_FW_Version}_${Required_FW_Revision}.7z.001"
+        FIRMWARE_FILE="trimui_tg5040.awimg"
+        message="Current   FW version: $Current_FW_Version - $Current_FW_Revision\nRequired FW version: $Required_FW_Version - $Required_FW_Revision\n \n \n"
+        crc_verified=false
 
-        button=$(/mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i "/mnt/SDCARD/trimui/firmwares/FW_Update_Instructions.png" -k "A B")
+        if [ ! -f "$FIRMWARE_PATH" ]; then
+            echo "Firmware file not found: $FIRMWARE_PATH"
+            /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i bg-exit.png -m "Firmware file not found, canceling update." -k A
+            exit 1
+        fi
 
-        if [ "$button" = "A" ]; then
+        if [ -f "/mnt/SDCARD/$FIRMWARE_FILE" ]; then
+            echo "Firmware file already exists, checking CRC..."
 
-            /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i "/mnt/SDCARD/trimui/firmwares/FW_Copy.png" -m "Please wait, copying Firmware v$Required_FW_Version - $Required_FW_Revision..."
+            message="${message}A firmware file is already present.\n"
 
-            FIRMWARE_PATH="/mnt/SDCARD/trimui/firmwares/trimui_tg5040_20240413_v1.0.4_hotfix6.7z"
-            FIRMWARE_FILE="trimui_tg5040.awimg"
-            /mnt/SDCARD/System/bin/7zz x "$FIRMWARE_PATH" -o"/mnt/SDCARD" -y
-            # cp "/mnt/SDCARD/trimui/firmwares/1.0.4 hotfix - 20240413.awimg" "/mnt/SDCARD/trimui_tg5040.awimg"
-            sync
-            sync
-
-            # Extract CRC from the 7z archive
-            ARCHIVE_CRC=$(/mnt/SDCARD/System/bin/7zz l "$FIRMWARE_PATH" -slt "$FIRMWARE_FILE" | grep "CRC = " | awk '{print $3}' | tr 'a-f' 'A-F')
-
-            # Calculate CRC of the extracted file
-            EXTRACTED_CRC=$(crc32 "/mnt/SDCARD/trimui_tg5040.awimg" | awk '{print $1}' | tr 'a-f' 'A-F')
-
-            # Compare the CRC values
-            if [ "$ARCHIVE_CRC" == "$EXTRACTED_CRC" ]; then
-                echo "FW CRC check passed: $EXTRACTED_CRC"
+            if check_firmware_crc "$FIRMWARE_PATH" "$FIRMWARE_FILE"; then
+                echo "CRC check on existing file is OK. Skipping extraction."
+                crc_verified=true
             else
-                echo "CRC check failed: Archive CRC = $ARCHIVE_CRC, Extracted CRC = $EXTRACTED_CRC"
-                /mnt/SDCARD/System/usr/trimui/scripts/infoscreen.sh -i bg-exit.png -m "Firmware CRC check has failed, canceling update." -k A
+                echo "CRC check on existing file failed. Re-extracting."
+                rm -f "/mnt/SDCARD/$FIRMWARE_FILE"
+            fi
+        fi
+
+        if [ "$crc_verified" = false ]; then
+            message="${message}\n \n \nExtracting new firmware v$Required_FW_Version..."
+            pkill presenter
+            sleep 0.5
+            /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -sp -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Wait.jpg" &
+
+            /mnt/SDCARD/System/usr/trimui/scripts/cpufreq.sh ondemand 3 7
+            if ! /mnt/SDCARD/System/bin/7zz x "$FIRMWARE_PATH" -o"/mnt/SDCARD" -y; then
+                echo "Failed to extract firmware"
+                pkill presenter
+                sleep 0.5
+                message="${message}\nFirmware extraction failed, canceling update.\n \nPress X to diagnose."
+                /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Fail.jpg" -k rout A "" -k rout X ""
+                rm "/mnt/SDCARD/$FIRMWARE_FILE"
+                diagnose
+                exit 1
+            fi
+            message="${message}OK\n"
+            pkill presenter
+            sleep 0.5
+            sync
+
+            if check_firmware_crc "$FIRMWARE_PATH" "$FIRMWARE_FILE"; then
+                crc_verified=true
+            else
+                message="${message}\nFirmware CRC check has failed, canceling update.\nPress X to diagnose."
+                pkill presenter
+                sleep 0.5
+                /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Fail.jpg" -k rout A "" -k rout X ""
+                rm "/mnt/SDCARD/$FIRMWARE_FILE"
+                diagnose
+                exit 1
+            fi
+        fi
+
+        if [ "$crc_verified" = true ]; then
+
+            pkill presenter
+            sleep 0.5
+
+            message="${message}\nReady for update!\n \nPlease read instructions at the right\nto launch your firmware upgrade."
+
+            /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$message" -fs 12 -fi 0 -p top-left -fb -ff "/mnt/SDCARD/Themes/CrossMix - OS/wqy-microhei.ttf" -i "/mnt/SDCARD/trimui/firmwares/FW_Screen_Ready.jpg" -k rout A "" -k rin B ""
+
+            if [ "$?" -eq 14 ]; then # A has been pressed
+                sleep 1
+                pkill presenter
+                sync
+                /usr/trimui/bin/kill_apps.sh
+                sleep 30
+                poweroff
+                sleep 30
+                exit
+            else
+                pkill presenter
+                exit
             fi
 
-            sleep 1
-            sync
-            poweroff
-            sleep 30
-            exit
-        else
-            rm -f "/mnt/SDCARD/trimui_tg5040.awimg"
         fi
 
     else
