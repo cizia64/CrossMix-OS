0e18c70cd28190f91b96146f2bc73eddec590b6f
cizia64 <cizia64@gmail.com>
2025-08-08 02:03:39 +0200

    Update FolderOverrideFinder.sh
    execution time optimization

--- System/usr/trimui/scripts/FolderOverrideFinder.sh
+++ System/usr/trimui/scripts/FolderOverrideFinder.sh
@@ -8,27 +8,31 @@ rom_path_after_roms=${ROM_PATH#*/Roms/}
 subdir_count=$(echo "$rom_path_after_roms" | grep -o '/' | wc -l)
 
 if [ "$subdir_count" -gt 1 ]; then
-    ROM_DIRECTORY=$(dirname "$ROM_PATH")
+    ROM_DIRECTORY=${ROM_PATH%/*}
+    first_subdir=${rom_path_after_roms%%/*}
     ROM_FILENAME=$(basename "$1")
     ROM_FILENAME_NOEXT=${ROM_FILENAME%.*}
-    first_subdir=$(echo "$rom_path_after_roms" | cut -d'/' -f1)
     SPOOFING_DIRECTORY="/tmp/folderspoof/$ROM_FILENAME_NOEXT/$first_subdir"
-    echo $SPOOFING_DIRECTORY >"/tmp/${ROM_FILENAME}_spoofing_directory.txt"
+
     echo "Subdirectory from $first_subdir detected !"
+
     mkdir -p "$SPOOFING_DIRECTORY"
-    mount -o bind "$ROM_DIRECTORY" "$SPOOFING_DIRECTORY" >/tmp/txtfolderspoof
+    mount -o bind "$ROM_DIRECTORY" "$SPOOFING_DIRECTORY"
 
     POST_RUN_SCRIPT="/tmp/folderspoof/${ROM_FILENAME_NOEXT}_postrun.sh"
-    {
-        echo "while kill -0 "$$" 2>/dev/null; do"
-        echo "sleep 2"
-        echo "done"
-        echo "sync"
-        echo "umount \"$SPOOFING_DIRECTORY\""
-        echo "if [ \$? = 0 ]; then"
-        echo "rm -rf \"$SPOOFING_DIRECTORY\""
-        echo "fi"
-    } >"$POST_RUN_SCRIPT"
+    cat <<EOF >"$POST_RUN_SCRIPT"
+while kill -0 "$$" 2>/dev/null; do
+  sleep 2
+done
+sync
+umount "$SPOOFING_DIRECTORY"
+if [ \$? = 0 ]; then
+  sleep 1
+  rmdir "$SPOOFING_DIRECTORY"
+  rmdir "/tmp/folderspoof/$ROM_FILENAME_NOEXT"
+fi
+rm "${POST_RUN_SCRIPT}"
+EOF
 
     chmod a+x "$POST_RUN_SCRIPT"
     "$POST_RUN_SCRIPT" &
