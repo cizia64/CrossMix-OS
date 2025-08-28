0901347cf5747b45e6fe3dbe6c12b40dd90ec4d6
cizia64 <cizia64@gmail.com>
2025-08-28 22:34:51 +0200

    Update launch.sh
    Add Y menu launcher option to be able
    - to stop smartledd background process quickly
    - enable or disable smartledd background service without requiring to go in System Tools

--- Apps/SmartLed/launch.sh
+++ Apps/SmartLed/launch.sh
@@ -4,28 +4,53 @@ export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/usr/trimui/lib:$LD_LIBRARY_PATH"
 
 cd $(dirname "$0")
 
-# Check if deamon is running
-if pgrep -f "smartledd" >/dev/null; then
-    echo "smartledd is already running"
-    killall smartledd
-else
-    
-    # check if SmartLed is set to automatic start
-    SmartLed_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMARTLED"]' "/mnt/SDCARD/System/etc/crossmix.json")
-    echo "SmartLed_enabled $SmartLed_enabled"
-    if ! [ "$SmartLed_enabled" -eq 1 ]; then
-        SmartLed_choice=$(/mnt/SDCARD/System/bin/presenter --file /mnt/SDCARD/System/resources/SmartLed_daemon_choice.json --confirm-button A --no-wrap)
-        if [ "$SmartLed_choice" = "2" ]; then
-            json_file="/mnt/SDCARD/System/etc/crossmix.json"
-            jq '. += {"SMARTLED": 1}' "$json_file" >"/tmp/json_file.tmp" && mv "/tmp/json_file.tmp" "$json_file"
-            # we modify the DB entries to reflect the current state
-            mainui_state_update.sh "SMARTLED" "enabled"
-            
-        fi
-        
-    fi
+#!/bin/sh
+
+/mnt/SDCARD/System/usr/trimui/scripts/button_state.sh Y
+if [ $? -eq 10 ]; then
+	CHOICE=$(/mnt/SDCARD/System/bin/selector -fs 120 -c \
+		"Stop SmartLed" \
+		"----------------------------" \
+		"Enable SmartLed at boot" \
+		"Disable SmartLed at boot")
+
+	CHOICE=$(printf '%s\n' "$CHOICE" | sed 's/^.*: //')
+
+	if [ "$CHOICE" = "Stop SmartLed" ]; then
+		killall smartledd
+
+	elif [ "$CHOICE" = "Enable SmartLed at boot" ]; then
+		sh "/mnt/SDCARD/Apps/SystemTools/Menu/CONTROLS##SMARTLED (state)/SmartLed - enable.sh"
+		if ! pgrep -f "smartledd" >/dev/null; then
+			./smartledd &
+		fi
+	elif [ "$CHOICE" = "Disable SmartLed at boot" ]; then
+		sh "/mnt/SDCARD/Apps/SystemTools/Menu/CONTROLS##SMARTLED (state)/SmartLed - disable.sh"
+	fi
+	exit
 fi
 
+# Check if deamon is running
+if pgrep -f "smartledd" >/dev/null; then
+	echo "smartledd is already running"
+	killall smartledd
+else
+
+	# check if SmartLed is set to automatic start
+	SmartLed_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMARTLED"]' "/mnt/SDCARD/System/etc/crossmix.json")
+	echo "SmartLed_enabled $SmartLed_enabled"
+	if ! [ "$SmartLed_enabled" -eq 1 ]; then
+		SmartLed_choice=$(/mnt/SDCARD/System/bin/presenter --file /mnt/SDCARD/System/resources/SmartLed_daemon_choice.json --confirm-button A --no-wrap)
+		if [ "$SmartLed_choice" = "2" ]; then
+			json_file="/mnt/SDCARD/System/etc/crossmix.json"
+			jq '. += {"SMARTLED": 1}' "$json_file" >"/tmp/json_file.tmp" && mv "/tmp/json_file.tmp" "$json_file"
+			# we modify the DB entries to reflect the current state
+			mainui_state_update.sh "SMARTLED" "enabled"
+
+		fi
+
+	fi
+fi
 
 ./smartledd &
 
