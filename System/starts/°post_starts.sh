ad2661bb6217c4710d2af60d54aeed08a53aef43
cizia64 <cizia64@gmail.com>
2025-09-01 01:55:40 +0200

    [Feature] Update notifications implementation (#483)

--- System/starts/°post_starts.sh
+++ System/starts/°post_starts.sh
@@ -1,9 +1,30 @@
+#!/bin/sh
+
 PATH="/mnt/SDCARD/System/bin:$PATH"
 export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:/mnt/SDCARD/System/lib/samba:/mnt/SDCARD/Apps/PortMaster/PortMaster:/usr/trimui/lib:$LD_LIBRARY_PATH"
 
+eval $(
+	/mnt/SDCARD/System/bin/jq -r '
+        @sh "
+        NightMode=\(.["NightMode"]//0)
+        SWAP_AB_enabled=\(.["SWAP A B"]//0)
+        TELNET_enabled=\(.["TELNET"]//0)
+        Syncthing_enabled=\(.["Syncthing"]//0)
+        smb_enabled=\(.["SMB"]//0)
+        smb_secure_enabled=\(.["SMB_secure"]//0)
+        Tailscale_enabled=\(.["Tailscale"]//0)
+        VNC_enabled=\(.["VNC"]//0)
+        IngameMenu_enabled=\(.["IN GAME MENU"]//1)
+        SmartLed_enabled=\(.["SMARTLED"]//0)
+        StartupApp=\(.["STARTUP APP"]//"MainUI")
+        CheckUpdate=\(.["CHECK UPDATE"]//1)
+        "' "/mnt/SDCARD/System/etc/crossmix.json"
+)
+
+# --- Functions ---
 start_thd_listener() {
 	(
-		NightMode=$(/mnt/SDCARD/System/bin/jq -r '.["NightMode"]' "/mnt/SDCARD/System/etc/crossmix.json")
+		NightMode=$(echo "$CONFIG_JSON" | /mnt/SDCARD/System/bin/jq -r '.["NightMode"]')
 
 		if [ "$NightMode" = "Configurator" ]; then
 			if ! pgrep -f nightmode_osdd >/dev/null; then
@@ -18,57 +39,13 @@ start_thd_listener() {
 }
 start_thd_listener
 
-if [ -f /tmp/device_changed ]; then
-	/mnt/SDCARD/System/usr/trimui/scripts/inputd_switcher.sh
-fi
-
-# Telnet service
-TELNET_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["TELNET"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$TELNET_enabled" -eq 1 ]; then
-	telnetd &
-fi
-
-# Syncthing service
-Syncthing_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["Syncthing"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$Syncthing_enabled" -eq 1 ]; then
-	CONFIGPATH=/mnt/SDCARD/System/etc/syncthing
-	/mnt/SDCARD/System/bin/syncthing serve --no-restart --no-upgrade --config="$CONFIGPATH" --data="$CONFIGPATH/data" &
-fi
-
-# SMB service
-smb_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMB"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$smb_enabled" -eq 1 ]; then
-	rm -rf /var/cache/samba /var/log/samba /var/lock/subsys /var/run/samba /var/lib/samba/
-	mkdir -p /var/cache/samba /var/log/samba /var/lock/subsys /var/run/samba /var/run/samba/locks /var/lib/samba/private
-
-	smb_secure_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMB_secure"]' "/mnt/SDCARD/System/etc/crossmix.json")
-	if [ "$smb_secure_enabled" -eq 1 ]; then
-		CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb-secure.conf"
-		echo -e "trimui\ntrimui\n" | smbpasswd -s -a root -c ${CONFIGFILE}
-	else
-		CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb.conf"
-	fi
-
-	wsddn --user root --unixd --smb-conf /mnt/SDCARD/System/etc/samba --log-level 0 &
-	smbd -s ${CONFIGFILE} -D &
-	nmbd -D --configfile="${CONFIGFILE}" &
-
-fi
-
-# Tailscale service
-Tailscale_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["Tailscale"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$Tailscale_enabled" -eq 1 ]; then
-	export STATE_DIRECTORY=/mnt/SDCARD/System/etc/tailscale
-	/mnt/SDCARD/System/bin/tailscaled --state="/mnt/SDCARD/System/etc/tailscale/tailscaled.state" &
-fi
-
 VNC_wait_for_MainUI() {
 	timeout=10
 	elapsed=0
 	while ! pgrep -x "MainUI" >/dev/null; do
 		sleep 0.5
 		elapsed=$(echo "$elapsed + 0.5" | bc)
-		if (($(echo "$elapsed >= $timeout" | bc))); then
+		if [ "$(echo "$elapsed >= $timeout" | bc)" -eq 1 ]; then
 			echo "Error: MainUI did not start within $timeout seconds."
 			return 1
 		fi
@@ -81,32 +58,7 @@ VNC_wait_for_MainUI() {
 	return 0
 }
 
-# VNC service
-VNC_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["VNC"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$VNC_enabled" -eq 1 ]; then
-	# Run the function in the background
-	VNC_wait_for_MainUI &
-fi
-
-# Avahi (DNS name) service
-/usr/sbin/avahi-daemon --file=/mnt/SDCARD/System/etc/avahi/avahi-daemon.conf --no-drop-root -D &
-
-# TrimUI In Game Menu state
-IngameMenu_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["IN GAME MENU"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$IngameMenu_enabled" -eq 0 ]; then
-	mkdir -p /tmp/trimui_ra64/
-	touch /tmp/trimui_ra64/disable_tmenu
-fi
-
-# SmartLed
-SmartLed_enabled=$(/mnt/SDCARD/System/bin/jq -r '.["SMARTLED"]' "/mnt/SDCARD/System/etc/crossmix.json")
-if [ "$SmartLed_enabled" -eq 1 ]; then
-	cd /mnt/SDCARD/Apps/SmartLed
-	./smartledd &
-fi
-
 wifi_workaround() {
-
 	TIMEOUT=8                    # in seconds
 	INTERVAL=0.5                 # in seconds
 	MAX_RETRIES=$((TIMEOUT * 2)) # 0.5s interval → 2 retries per second
@@ -156,10 +108,80 @@ wifi_workaround() {
 	else
 		echo "[INFO] IP address already present on wlan0."
 	fi
+	if [ "$CheckUpdate" -eq 1 ]; then
+		/mnt/SDCARD/System/usr/trimui/scripts/update_notification.sh &
+	fi
+
 }
 
-# hardwareservice race condition workaround for wifi initialization
+# --- Launching background services ---
+
+if [ -f /tmp/device_changed ]; then
+	/mnt/SDCARD/System/usr/trimui/scripts/inputd_switcher.sh
+fi
+
+if [ "$SWAP_AB_enabled" -eq 1 ]; then
+	mkdir -p /var/trimui_inputd
+	touch /var/trimui_inputd/swap_ab
+fi
+
+if [ "$TELNET_enabled" -eq 1 ]; then
+	telnetd &
+fi
+
+if [ "$Syncthing_enabled" -eq 1 ]; then
+	CONFIGPATH=/mnt/SDCARD/System/etc/syncthing
+	/mnt/SDCARD/System/bin/syncthing serve --no-restart --no-upgrade --config="$CONFIGPATH" --data="$CONFIGPATH/data" &
+fi
+
+if [ "$smb_enabled" -eq 1 ]; then
+	rm -rf /var/cache/samba /var/log/samba /var/lock/subsys /var/run/samba /var/lib/samba/
+	mkdir -p /var/cache/samba /var/log/samba /var/lock/subsys /var/run/samba /var/run/samba/locks /var/lib/samba/private
+
+	if [ "$smb_secure_enabled" -eq 1 ]; then
+		CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb-secure.conf"
+		echo -e "trimui\ntrimui\n" | smbpasswd -s -a root -c ${CONFIGFILE}
+	else
+		CONFIGFILE="/mnt/SDCARD/System/etc/samba/smb.conf"
+	fi
+
+	wsddn --user root --unixd --smb-conf /mnt/SDCARD/System/etc/samba --log-level 0 &
+	smbd -s ${CONFIGFILE} -D &
+	nmbd -D --configfile="${CONFIGFILE}" &
+fi
+
+if [ "$Tailscale_enabled" -eq 1 ]; then
+	export STATE_DIRECTORY=/mnt/SDCARD/System/etc/tailscale
+	/mnt/SDCARD/System/bin/tailscaled --state="/mnt/SDCARD/System/etc/tailscale/tailscaled.state" &
+fi
+
+if [ "$VNC_enabled" -eq 1 ]; then
+	VNC_wait_for_MainUI &
+fi
+
+/usr/sbin/avahi-daemon --file=/mnt/SDCARD/System/etc/avahi/avahi-daemon.conf --no-drop-root -D &
+
+if [ "$IngameMenu_enabled" -eq 0 ]; then
+	mkdir -p /tmp/trimui_ra64/
+	touch /tmp/trimui_ra64/disable_tmenu
+fi
+
+if [ "$SmartLed_enabled" -eq 1 ]; then
+	cd /mnt/SDCARD/Apps/SmartLed || exit
+	./smartledd &
+fi
+
+if [ ! -f "/mnt/SDCARD/trimui/app/cmd_to_run.sh" ]; then
+	if [ "$StartupApp" = "Activities - List" ]; then
+		cp "/mnt/SDCARD/Apps/Activities/launch.sh" "/mnt/SDCARD/trimui/app/cmd_to_run.sh"
+	elif [ "$StartupApp" = "Activities - Details" ]; then
+		cp "/mnt/SDCARD/Apps/Activities/launch - details.sh" "/mnt/SDCARD/trimui/app/cmd_to_run.sh"
+	elif [ "$StartupApp" = "RetroArch" ]; then
+		cp /mnt/SDCARD/Apps/RetroArch/launch.sh "/mnt/SDCARD/trimui/app/cmd_to_run.sh"
+	fi
+fi
+
 wifi_value=$(/usr/trimui/bin/systemval wifi)
 if [ "$wifi_value" -eq 1 ]; then
-	wifi_workaround & # for debugging: wifi_workaround >>/tmp/wifi_debug.log 2>&1 &
+	wifi_workaround &
 fi
