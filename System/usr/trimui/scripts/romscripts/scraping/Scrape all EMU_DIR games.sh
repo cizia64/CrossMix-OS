402a830b513d61f2e9c4d554413863c1e00c52cc
cizia64 <cizia64@gmail.com>
2025-09-01 10:00:51 +0200

    [Feature] RomScripts (#485)

---
+++ System/usr/trimui/scripts/romscripts/scraping/Scrape all EMU_DIR games.sh
@@ -0,0 +1,3 @@
+# "/mnt/SDCARD/System/usr/trimui/scripts/scraper/scrap_screenscraper.sh"  "$(basename "$EMU_DIR")" "$(basename "$ROM_REAL_PATH")" & 
+
+/mnt/SDCARD/System/bin/text_viewer -s "/mnt/SDCARD/System/usr/trimui/scripts/scraper/scrap_screenscraper.sh $(basename "$EMU_DIR")" -f 25 -t "                            ScreenScraper by Schmurtz.                     (Press B to stop)"
---
+++ System/usr/trimui/scripts/romscripts/scraping/Scrape this game.sh
@@ -0,0 +1,2 @@
+
+"/mnt/SDCARD/System/usr/trimui/scripts/scraper/scrap_screenscraper.sh"  "$(basename "$EMU_DIR")" "$(basename "$ROM_REAL_PATH")" & 
