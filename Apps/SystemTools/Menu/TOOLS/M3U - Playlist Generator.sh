952a21d7b3a991c3d547c1a4a9eaecee3349d670
cizia64 <cizia64@gmail.com>
2025-09-05 16:35:42 +0200

    Use updated infoscreen2.sh
    No more sleep/pkill necessary in most cases

--- Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
+++ Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
@@ -11,14 +11,12 @@ for dir in $DIR_LIST; do
     /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$Message\n$dir playlist(s) creation..." -fi 0 -p top-left -fb -sp &
 
     COUNT=$(python3.11 "/mnt/SDCARD/System/usr/trimui/scripts/M3U - Playlist Generator.py" -md "/mnt/SDCARD/Roms/$dir" | grep '^TOTAL_M3U_CREATED=' | cut -d= -f2)
-    sleep 1.5
-    pkill presenter
-	sleep 0.5
 
     # If COUNT is empty, set to 0
     COUNT=${COUNT:-0}
     TOTAL=$((TOTAL + COUNT))
     Message="$Message\n$dir playlist(s) created: $COUNT"
+    sleep 1.5
 
 done
 
@@ -30,4 +28,3 @@ Message="$Message \nTotal playlist(s) created: $TOTAL\n"
 Message="$Message \n--------------------------------------"
 
 /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$Message" -fi 0 -p top-left -k rin B "Exit"
-
