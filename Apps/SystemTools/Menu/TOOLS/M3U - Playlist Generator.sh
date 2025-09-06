4d5158e80ff444a35ac12c9560c4715aef3976e7
cizia64 <cizia64@gmail.com>
2025-09-06 03:04:25 +0200

    M3U - Playlist Generator.sh: Refresh roms
    When at least one playlist is created we refresh roms in each folder

--- Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
+++ Apps/SystemTools/Menu/TOOLS/M3U - Playlist Generator.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-export LD_LIBRARY_PATH=/mnt/SDCARD/Apps/TubeExJuk:/mnt/SDCARD/System/lib:/lib64:/usr/trimui/lib:/usr/lib
+export LD_LIBRARY_PATH=/mnt/SDCARD/System/lib:/lib64:/usr/trimui/lib:/usr/lib
 export PATH=$PATH:/mnt/SDCARD/System/bin
 
 DIR_LIST="PS SEGACD NEOCD PCE PCFX AMIGA"
@@ -12,19 +11,22 @@ for dir in $DIR_LIST; do
 
     COUNT=$(python3.11 "/mnt/SDCARD/System/usr/trimui/scripts/M3U - Playlist Generator.py" -md "/mnt/SDCARD/Roms/$dir" | grep '^TOTAL_M3U_CREATED=' | cut -d= -f2)
 
-    # If COUNT is empty, set to 0
     COUNT=${COUNT:-0}
     TOTAL=$((TOTAL + COUNT))
     Message="$Message\n$dir playlist(s) created: $COUNT"
     sleep 1.5
-
 done
 
-# Final message:
 Message="$Message\n"
-Message="$Message\n \n--------------------------------------\n"
+Message="$Message\n \n--------------------------------------\n"
 Message="$Message Finished.\n"
 Message="$Message \nTotal playlist(s) created: $TOTAL\n"
 Message="$Message \n--------------------------------------"
+
+if [ "$TOTAL" -gt 0 ]; then
+    for dir in $DIR_LIST; do
+        rm -f "/mnt/SDCARD/Roms/$dir/${dir}_cache7.db"
+    done
+fi
 
 /mnt/SDCARD/System/usr/trimui/scripts/infoscreen2.sh -m "$Message" -fi 0 -p top-left -k rin B "Exit"
