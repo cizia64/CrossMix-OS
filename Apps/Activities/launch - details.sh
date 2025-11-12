ee5a6800ace6c9229934c0fef6935bbbdef1ec22
cizia64 <cizia64@gmail.com>
2025-11-12 02:31:11 +0100

    Fix Activities in list mode at boot

--- Apps/Activities/launch - details.sh
+++ Apps/Activities/launch - details.sh
@@ -11,4 +11,4 @@ sed -iE 's/^skins_theme=.*$/skins_theme='"${skins#*Themes/}"'
   s/^backgrounds_theme=.*$/backgrounds_theme='"$backgrounds/" data/config.ini
 sync
 
-/mnt/SDCARD/System/bin/activities gui # -last
+/mnt/SDCARD/System/bin/activities gui -last
